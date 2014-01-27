8a8bbf2a82ca1fa13863c296217a697d7bfd8335
Brad Harding <brad@doomretro.com>
2014-01-27 17:41:21 +1100

    Fix potential crash when removing thinker.
    Removing the thinker here was causing a crash, but only when debugging.

--- src/p_tick.c
+++ src/p_tick.c
@@ -100,7 +100,6 @@ void P_RunThinkers(void)
             // time to remove it
             currentthinker->next->prev = currentthinker->prev;
             currentthinker->prev->next = currentthinker->next;
-            Z_Free(currentthinker);
         }
         else
         {
