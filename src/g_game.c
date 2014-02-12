bradharding/doomretro

commit dfbcfbe8dfec1d4ba302d176817df0d8ef6b74c6

Don't go widescreen in options menu when no game
Don't switch to widescreen in the options menu when there's no game
playing, otherwise TITLEPIC, INTERPIC will be cropped

src/i_video.c
@@ -819,7 +819,6 @@ void ToggleWideScreen(boolean toggle)
             return;
 
         widescreen = true;
-        returntowidescreen = false;
 
         width = screen->w + 1;
         height = screen->h + (int)((double)screen->h * 32 / (ORIGINALHEIGHT - 32) + 1.5);
@@ -832,6 +831,7 @@ void ToggleWideScreen(boolean toggle)
         width = height * 4 / 3;
         width += (width & 1);
     }
+    returntowidescreen = false;
 
     screenbuffer = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 8, 0, 0, 0, 0);
     pitch = screenbuffer->pitch;

src/m_menu.c
@@ -1569,7 +1569,7 @@ void M_SizeDisplay(int choice)
     switch (choice)
     {
         case 0:
-            if (widescreen)
+            if (widescreen || (returntowidescreen && gamestate != GS_LEVEL))
             {
                 screenSize--;
                 S_StartSound(NULL, sfx_stnmov);
@@ -1588,11 +1588,16 @@ void M_SizeDisplay(int choice)
         case 1:
             if (screenSize == 7 && fullscreen)
             {
-                ToggleWideScreen(true);
-                if (!widescreen)
+                if (gamestate != GS_LEVEL)
+                    returntowidescreen = true;
+                else
                 {
-                    screenblocks++;
-                    R_SetViewSize(screenblocks);
+                    ToggleWideScreen(true);
+                    if (!widescreen)
+                    {
+                        screenblocks++;
+                        R_SetViewSize(screenblocks);
+                    }
                 }
                 screenSize++;
                 S_StartSound(NULL, sfx_stnmov);

commit 12965f93ee72d570a5d172bdc1767c050ac3556f

Fix not returning to widescreen
Commit dfbcfbe8dfec1d4ba302d176817df0d8ef6b74c6 caused game not to
return to widescreen after displaying a 4:3 screen.

src/g_game.c
@@ -1348,8 +1348,8 @@ void G_DoCompleted(void)
 
     if (widescreen)
     {
-        returntowidescreen = true;
         ToggleWideScreen(false);
+        returntowidescreen = true;
     }
 
     // [BH] allow the exit switch to turn on before the screen wipes

src/i_video.c
@@ -813,7 +813,7 @@ static void SetVideoMode(void)
 
 void ToggleWideScreen(boolean toggle)
 {
-    if ((double)screenwidth / screenheight < 1.6)
+    if ((double)screen->w / screen->h < 1.6)
         return;
 
     if (toggle)

src/m_menu.c
@@ -1128,8 +1128,8 @@ void M_DrawReadThis(void)
         inhelpscreens = true;
         if (widescreen)
         {
-            returntowidescreen = true;
             ToggleWideScreen(false);
+            returntowidescreen = true;
         }
         if (automapactive)
             V_FillRect(0, 0, 0, SCREENWIDTH, SCREENHEIGHT, 245);
@@ -1431,8 +1431,8 @@ void M_EndGameResponse(int key)
     endinggame = true;
     if (widescreen)
     {
-        returntowidescreen = true;
         ToggleWideScreen(false);
+        returntowidescreen = true;
     }
     D_StartTitle();
 }