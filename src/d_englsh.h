454b18d2f9272b328fcd19337376ace10b094e54
Brad Harding <brad@doomretro.com>
2014-04-29 17:57:52 +1000

    Change "CHANGING" to "WARPING" in IDCLEV message

--- src/d_englsh.h
+++ src/d_englsh.h
@@ -402,7 +402,7 @@ along with DOOM RETRO. If not, see http://www.gnu.org/licenses/.
 
 #define STSTR_CHOPPERS          "\"...doesn't suck.\" - GM"
 
-#define STSTR_CLEV              "Changing to %s..."
+#define STSTR_CLEV              "Warping to %s..."
 #define STSTR_CLEVSAME          "Restarting %s..."
 
 #define STSTR_MYPOS             "angle = %i%c  x,y,z = (%i,%i,%i)"
