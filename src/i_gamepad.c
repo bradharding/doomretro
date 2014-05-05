bradharding/doomretro

commit b9091b809db97211196d542bead8763dbe737652

Miscellaneous optimizations to gamepad polling

src/i_gamepad.c
@@ -37,13 +37,12 @@ along with DOOM RETRO. If not, see http://www.gnu.org/licenses/.
 #pragma comment(lib, "XInput9_1_0.lib")
 #endif
 
+#include "d_main.h"
+#include "hu_stuff.h"
+#include "i_gamepad.h"
+#include "m_fixed.h"
 #include "SDL.h"
 #include "SDL_joystick.h"
-#include "d_main.h"
-#include "i_gamepad.h"
-#include "hu_stuff.h"
-
-#include "i_system.h"
 
 static SDL_Joystick *gamepad = NULL;
 
@@ -55,6 +54,7 @@ int gamepadthumbRX;
 boolean vibrate = false;
 
 void (*gamepadfunc)(void);
+void (*gamepadthumbsfunc)(short, short, short, short);
 
 extern int vibrationtics;
 extern boolean idclev;
@@ -63,8 +63,6 @@ extern boolean idbehold;
 
 void I_InitGamepad(void)
 {
-    gamepadfunc = I_PollDirectInputGamepad;
-
     if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) < 0)
         return;
     else
@@ -89,7 +87,17 @@ void I_InitGamepad(void)
             XINPUT_STATE state;
 
             if (XInputGetState(0, &state) == ERROR_SUCCESS)
+            {
                 gamepadfunc = I_PollXInputGamepad;
+                gamepadthumbsfunc = (gamepadlefthanded ? I_PollThumbs_XInput_LeftHanded :
+                    I_PollThumbs_XInput_RightHanded);
+            }
+            else
+            {
+                gamepadfunc = I_PollDirectInputGamepad;
+                gamepadthumbsfunc = (gamepadlefthanded ? I_PollThumbs_DirectInput_LeftHanded :
+                    I_PollThumbs_DirectInput_RightHanded);
+            }
 
             SDL_JoystickEventState(SDL_ENABLE);
         }
@@ -110,9 +118,21 @@ static int __inline clamp(int value, int deadzone)
 {
     if (value > -deadzone && value < deadzone)
         return 0;
-    if (value == -32768)
-        value = -32767;
-    return value;
+    return MIN(-32767, value);
+}
+
+void I_PollThumbs_DirectInput_RightHanded(short LX, short LY, short RX, short RY)
+{
+    gamepadthumbLX = clamp(SDL_JoystickGetAxis(gamepad, LX), GAMEPAD_LEFT_THUMB_DEADZONE);
+    gamepadthumbLY = clamp(SDL_JoystickGetAxis(gamepad, LY), GAMEPAD_LEFT_THUMB_DEADZONE);
+    gamepadthumbRX = clamp(SDL_JoystickGetAxis(gamepad, RX), GAMEPAD_RIGHT_THUMB_DEADZONE);
+}
+
+void I_PollThumbs_DirectInput_LeftHanded(short LX, short LY, short RX, short RY)
+{
+    gamepadthumbLX = clamp(SDL_JoystickGetAxis(gamepad, RX), GAMEPAD_RIGHT_THUMB_DEADZONE);
+    gamepadthumbLY = clamp(SDL_JoystickGetAxis(gamepad, RY), GAMEPAD_RIGHT_THUMB_DEADZONE);
+    gamepadthumbRX = clamp(SDL_JoystickGetAxis(gamepad, LX), GAMEPAD_LEFT_THUMB_DEADZONE);
 }
 
 void I_PollDirectInputGamepad(void)
@@ -120,62 +140,26 @@ void I_PollDirectInputGamepad(void)
     if (gamepad)
     {
         event_t         ev;
-        int             hat;
+        int             hat = SDL_JoystickGetHat(gamepad, 0);
 
-        if (gamepadlefthanded)
-        {
-            gamepadthumbLX = clamp(SDL_JoystickGetAxis(gamepad, 2),
-                                   GAMEPAD_RIGHT_THUMB_DEADZONE);
-            gamepadthumbLY = clamp(SDL_JoystickGetAxis(gamepad, 3),
-                                   GAMEPAD_RIGHT_THUMB_DEADZONE);
-            gamepadthumbRX = clamp(SDL_JoystickGetAxis(gamepad, 0),
-                                   GAMEPAD_LEFT_THUMB_DEADZONE);
-        }
-        else
-        {
-            gamepadthumbLX = clamp(SDL_JoystickGetAxis(gamepad, 0),
-                                   GAMEPAD_LEFT_THUMB_DEADZONE);
-            gamepadthumbLY = clamp(SDL_JoystickGetAxis(gamepad, 1),
-                                   GAMEPAD_LEFT_THUMB_DEADZONE);
-            gamepadthumbRX = clamp(SDL_JoystickGetAxis(gamepad, 2),
-                                   GAMEPAD_RIGHT_THUMB_DEADZONE);
-        }
+        gamepadthumbsfunc(0, 1, 2, 3);
 
-        gamepadbuttons = 0;
-        if (SDL_JoystickGetButton(gamepad, 0))
-            gamepadbuttons |= GAMEPAD_X;
-        if (SDL_JoystickGetButton(gamepad, 1))
-            gamepadbuttons |= GAMEPAD_A;
-        if (SDL_JoystickGetButton(gamepad, 2))
-            gamepadbuttons |= GAMEPAD_B;
-        if (SDL_JoystickGetButton(gamepad, 3))
-            gamepadbuttons |= GAMEPAD_Y;
-        if (SDL_JoystickGetButton(gamepad, 4))
-            gamepadbuttons |= GAMEPAD_LEFT_SHOULDER;
-        if (SDL_JoystickGetButton(gamepad, 5))
-            gamepadbuttons |= GAMEPAD_RIGHT_SHOULDER;
-        if (SDL_JoystickGetButton(gamepad, 6))
-            gamepadbuttons |= GAMEPAD_LEFT_TRIGGER;
-        if (SDL_JoystickGetButton(gamepad, 7))
-            gamepadbuttons |= GAMEPAD_RIGHT_TRIGGER;
-        if (SDL_JoystickGetButton(gamepad, 8))
-            gamepadbuttons |= GAMEPAD_BACK;
-        if (SDL_JoystickGetButton(gamepad, 9))
-            gamepadbuttons |= GAMEPAD_START;
-        if (SDL_JoystickGetButton(gamepad, 10))
-            gamepadbuttons |= GAMEPAD_LEFT_THUMB;
-        if (SDL_JoystickGetButton(gamepad, 11))
-            gamepadbuttons |= GAMEPAD_RIGHT_THUMB;
-
-        hat = SDL_JoystickGetHat(gamepad, 0);
-        if (hat & SDL_HAT_UP)
-            gamepadbuttons |= GAMEPAD_DPAD_UP;
-        if (hat & SDL_HAT_RIGHT)
-            gamepadbuttons |= GAMEPAD_DPAD_RIGHT;
-        if (hat & SDL_HAT_DOWN)
-            gamepadbuttons |= GAMEPAD_DPAD_DOWN;
-        if (hat & SDL_HAT_LEFT)
-            gamepadbuttons |= GAMEPAD_DPAD_LEFT;
+        gamepadbuttons = (GAMEPAD_X * SDL_JoystickGetButton(gamepad, 0) |
+            GAMEPAD_A * SDL_JoystickGetButton(gamepad, 1) |
+            GAMEPAD_B * SDL_JoystickGetButton(gamepad, 2) |
+            GAMEPAD_Y * SDL_JoystickGetButton(gamepad, 3) |
+            GAMEPAD_LEFT_SHOULDER * SDL_JoystickGetButton(gamepad, 4) |
+            GAMEPAD_RIGHT_SHOULDER * SDL_JoystickGetButton(gamepad, 5) |
+            GAMEPAD_LEFT_TRIGGER * SDL_JoystickGetButton(gamepad, 6) |
+            GAMEPAD_RIGHT_TRIGGER * SDL_JoystickGetButton(gamepad, 7) |
+            GAMEPAD_BACK * SDL_JoystickGetButton(gamepad, 8) |
+            GAMEPAD_START * SDL_JoystickGetButton(gamepad, 9) |
+            GAMEPAD_LEFT_THUMB * SDL_JoystickGetButton(gamepad, 10) |
+            GAMEPAD_RIGHT_THUMB * SDL_JoystickGetButton(gamepad, 11) |
+            GAMEPAD_DPAD_UP * (hat & SDL_HAT_UP) |
+            GAMEPAD_DPAD_RIGHT * (hat & SDL_HAT_RIGHT) |
+            GAMEPAD_DPAD_DOWN * (hat & SDL_HAT_DOWN) |
+            GAMEPAD_DPAD_LEFT * (hat & SDL_HAT_LEFT));
 
         if (gamepadbuttons)
         {
@@ -204,35 +188,37 @@ void XInputVibration(int left, int right)
     XInputSetState(0, &vibration);
 }
 
+void I_PollThumbs_XInput_RightHanded(short LX, short LY, short RX, short RY)
+{
+    gamepadthumbLX = clamp(LX, GAMEPAD_LEFT_THUMB_DEADZONE);
+    gamepadthumbLY = -clamp(LY, GAMEPAD_LEFT_THUMB_DEADZONE);
+    gamepadthumbRX = clamp(RX, GAMEPAD_RIGHT_THUMB_DEADZONE);
+}
+
+void I_PollThumbs_XInput_LeftHanded(short LX, short LY, short RX, short RY)
+{
+    gamepadthumbLX = clamp(RX, GAMEPAD_RIGHT_THUMB_DEADZONE);
+    gamepadthumbLY = -clamp(RY, GAMEPAD_RIGHT_THUMB_DEADZONE);
+    gamepadthumbRX = clamp(LX, GAMEPAD_LEFT_THUMB_DEADZONE);
+}
+
 void I_PollXInputGamepad(void)
 {
     if (gamepad)
     {
         event_t         ev;
         XINPUT_STATE    state;
+        XINPUT_GAMEPAD  Gamepad;
 
         ZeroMemory(&state, sizeof(XINPUT_STATE));
         XInputGetState(0, &state);
+        Gamepad = state.Gamepad;
 
-        if (gamepadlefthanded)
-        {
-            gamepadthumbLX = clamp(state.Gamepad.sThumbRX, GAMEPAD_RIGHT_THUMB_DEADZONE);
-            gamepadthumbLY = -clamp(state.Gamepad.sThumbRY, GAMEPAD_RIGHT_THUMB_DEADZONE);
-            gamepadthumbRX = clamp(state.Gamepad.sThumbLX, GAMEPAD_LEFT_THUMB_DEADZONE);
-        }
-        else
-        {
-            gamepadthumbLX = clamp(state.Gamepad.sThumbLX, GAMEPAD_LEFT_THUMB_DEADZONE);
-            gamepadthumbLY = -clamp(state.Gamepad.sThumbLY, GAMEPAD_LEFT_THUMB_DEADZONE);
-            gamepadthumbRX = clamp(state.Gamepad.sThumbRX, GAMEPAD_RIGHT_THUMB_DEADZONE);
-        }
+        gamepadthumbsfunc(Gamepad.sThumbLX, Gamepad.sThumbLY, Gamepad.sThumbRX, Gamepad.sThumbRY);
 
-        gamepadbuttons = state.Gamepad.wButtons;
-
-        if (state.Gamepad.bLeftTrigger > GAMEPAD_TRIGGER_THRESHOLD)
-            gamepadbuttons |= GAMEPAD_LEFT_TRIGGER;
-        if (state.Gamepad.bRightTrigger > GAMEPAD_TRIGGER_THRESHOLD)
-            gamepadbuttons |= GAMEPAD_RIGHT_TRIGGER;
+        gamepadbuttons = (state.Gamepad.wButtons |
+            GAMEPAD_LEFT_TRIGGER * (state.Gamepad.bLeftTrigger > GAMEPAD_TRIGGER_THRESHOLD) |
+            GAMEPAD_RIGHT_TRIGGER * (state.Gamepad.bRightTrigger > GAMEPAD_TRIGGER_THRESHOLD));
 
         if (vibrationtics)
             if (!(--vibrationtics))

src/i_gamepad.h
@@ -73,6 +73,10 @@ void I_InitGamepad(void);
 void I_ShutdownGamepad(void);
 void I_PollDirectInputGamepad(void);
 void I_PollXInputGamepad(void);
+void I_PollThumbs_DirectInput_LeftHanded(short LX, short LY, short RX, short RY);
+void I_PollThumbs_DirectInput_RightHanded(short LX, short LY, short RX, short RY);
+void I_PollThumbs_XInput_LeftHanded(short LX, short LY, short RX, short RY);
+void I_PollThumbs_XInput_RightHanded(short LX, short LY, short RX, short RY);
 void(*gamepadfunc)(void);
 
 #endif

commit 1c5b988a83dc6710778156b67a00ad0833234599

Fix game exiting when no gamepad connected
Game would exit silently when no gamepad was connected, because
gamepadfunc() wasn't initialized and still being called. Caused by
change in b9091b809db97211196d542bead8763dbe737652.

src/i_gamepad.c
@@ -63,6 +63,8 @@ extern boolean idbehold;
 
 void I_InitGamepad(void)
 {
+    gamepadfunc = NULL;
+
     if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) < 0)
         return;
     else