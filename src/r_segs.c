e3512ccb316a114853bec7286c3a00a60cc09936
Brad Harding <brad@doomretro.com>
2014-05-08 10:36:03 +1000

    Fix some overflows by R_ScaleFromGlobalAngle()
    Setting max in this function to a higher value than 64 improves the
    "wobbly" vertexes that can occur, but this causes overflows to occur.
    Now that I've actually found an instance of this occurring (DOOM2.WAD,
    MAP06: The Crusher, angle = 0, x,y,z = (511, 797, -512)), I've lowered
    the max from 1024 to 512. I guess overflows will still occur, but until
    I find another one, this can stay.

--- src/r_segs.c
+++ src/r_segs.c
@@ -83,7 +83,7 @@ static fixed_t R_ScaleFromGlobalAngle(angle_t visangle)
     int         anglea = ANG90 + visangle - viewangle;
     int         angleb = ANG90 + visangle - rw_normalangle;
     int         den = FixedMul(rw_distance, finesine[anglea >> ANGLETOFINESHIFT]);
-    int         max = 1024 * FRACUNIT;          // [BH] fix wobbly vertexes
+    int         max = 512 * FRACUNIT;          // [BH] fix wobbly vertexes
     fixed_t     num = FixedMul(projectiony, finesine[angleb >> ANGLETOFINESHIFT]);
 
     return ((den >> 8) > 0 && den > (num >> 16) ? ((num = FixedDiv(num, den)) > max ? 
