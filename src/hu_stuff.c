bradharding/doomretro

commit 618751b00591dca4c4e2bc685dcef3636d3f6eee

Fix potential issues displaying keys in HUD
Use (B/R/Y)KEYA0 and (B/R/Y)SKUA0 instead of (B/R/Y)KEYB0 and
(B/R/Y)SKUB0 in HUD. Also, when determining what keys are needed in a
level, look at all the doors in the level as well as all the keys. This
fixes issue with Batman TC: since blue keys aren't spawned in a level,
and instead dropped by enemies, this causes wrong sprite to flash in HUD
when trying to open a locked blue door. This also fixes no blue key
given when using the IDKFA cheat.

src/hu_stuff.c
@@ -127,15 +127,14 @@ static struct
 static struct
 {
     char        *patchname;
-    int         mobjnum;
     patch_t     *patch;
 } keypic[NUMCARDS] = {
-    { "BKEYB0", MT_MISC4, NULL },
-    { "RKEYB0", MT_MISC5, NULL },
-    { "YKEYB0", MT_MISC6, NULL },
-    { "YSKUB0", MT_MISC7, NULL },
-    { "RSKUB0", MT_MISC8, NULL },
-    { "BSKUB0", MT_MISC9, NULL }
+    { "BKEYA0", NULL },
+    { "RKEYA0", NULL },
+    { "YKEYA0", NULL },
+    { "YSKUA0", NULL },
+    { "RSKUA0", NULL },
+    { "BSKUA0", NULL }
 };
 
 void HU_Init(void)
@@ -168,10 +167,7 @@ patch_t *HU_LoadHUDAmmoPatch(int ammopicnum)
 
 patch_t *HU_LoadHUDKeyPatch(int keypicnum)
 {
-    if (mobjinfo[keypic[keypicnum].mobjnum].flags & MF_SPECIAL)
-        return W_CacheLumpNum(W_GetNumForName(keypic[keypicnum].patchname), PU_CACHE);
-    else
-        return NULL;
+    return W_CacheLumpNum(W_GetNumForName(keypic[keypicnum].patchname), PU_CACHE);
 }
 
 void HU_Start(void)
@@ -402,7 +398,7 @@ static void HU_DrawHUD(void)
                             keyanimcounter = HUD_KEY_TICS;
                         }
                     }
-                    if (showkey && patch)
+                    if (showkey)
                         hudfunc(keypic_x - (SHORT(patch->width) + 6), HUD_KEYS_Y, patch, true);
                 }
             }
@@ -417,9 +413,8 @@ static void HU_DrawHUD(void)
                 {
                     patch_t     *patch = keypic[i].patch;
 
-                    if (patch)
-                        hudfunc(keypic_x + (SHORT(patch->width) + 6) * (cardsfound - plr->cards[i]),
-                            HUD_KEYS_Y, patch, true);
+                    hudfunc(keypic_x + (SHORT(patch->width) + 6) * (cardsfound - plr->cards[i]),
+                        HUD_KEYS_Y, patch, true);
                 }
         }
 

src/p_doors.c
@@ -196,7 +196,7 @@ int EV_DoLockedDoor(line_t *line, vldoor_e type, mobj_t *thing)
                     }
                     player->message = s_PD_BLUEO;
                 }
-                else
+                else if (player->cards[it_blueskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_blueskull)
                     {
@@ -223,7 +223,7 @@ int EV_DoLockedDoor(line_t *line, vldoor_e type, mobj_t *thing)
                     }
                     player->message = s_PD_REDO;
                 }
-                else
+                else if (player->cards[it_redskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_redskull)
                     {
@@ -250,7 +250,7 @@ int EV_DoLockedDoor(line_t *line, vldoor_e type, mobj_t *thing)
                     }
                     player->message = s_PD_YELLOWO;
                 }
-                else
+                else if (player->cards[it_yellowskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_yellowskull)
                     {
@@ -375,7 +375,7 @@ void EV_VerticalDoor(line_t *line, mobj_t *thing)
                     }
                     player->message = s_PD_BLUEK;
                 }
-                else
+                else if (player->cards[it_blueskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_blueskull)
                     {
@@ -405,7 +405,7 @@ void EV_VerticalDoor(line_t *line, mobj_t *thing)
                     }
                     player->message = s_PD_YELLOWK;
                 }
-                else
+                else if (player->cards[it_yellowskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_yellowskull)
                     {
@@ -435,7 +435,7 @@ void EV_VerticalDoor(line_t *line, mobj_t *thing)
                     }
                     player->message = s_PD_REDK;
                 }
-                else
+                else if (player->cards[it_redskull] == CARDNOTFOUNDYET)
                 {
                     if (!player->neededcardtics || player->neededcard != it_redskull)
                     {

src/p_inter.c
@@ -242,7 +242,7 @@ void P_InitCards(player_t *player)
 
     for (i = 0; i < numsectors; i++)
     {
-        mobj_t *thing = sectors[i].thinglist;
+        mobj_t  *thing = sectors[i].thinglist;
 
         while (thing)
         {
@@ -270,6 +270,36 @@ void P_InitCards(player_t *player)
                     break;
             }
             thing = thing->snext;
+        }
+    }
+
+    for (i = 0; i < numlines; i++)
+    {
+        line_t  *line = &lines[i];
+
+        switch (line->special)
+        {
+            case SR_OpenFastDoorStayOpenBlueKeyRequired:
+            case S1_OpenFastDoorStayOpenBlueKeyRequired:
+            case DR_OpenDoorWait4SecondsCloseBlueKeyRequired:
+            case D1_OpenDoorStayOpenBlueKeyRequired:
+                if (player->cards[it_blueskull] == CARDNOTINMAP)
+                    player->cards[it_bluecard] = CARDNOTFOUNDYET;
+                break;
+            case SR_OpenFastDoorStayOpenRedKeyRequired:
+            case S1_OpenFastDoorStayOpenRedKeyRequired:
+            case DR_OpenDoorWait4SecondsCloseRedKeyRequired:
+            case D1_OpenDoorStayOpenRedKeyRequired:
+                if (player->cards[it_redskull] == CARDNOTINMAP)
+                    player->cards[it_redcard] = CARDNOTFOUNDYET;
+                break;
+            case SR_OpenFastDoorStayOpenYellowKeyRequired:
+            case S1_OpenFastDoorStayOpenYellowKeyRequired:
+            case DR_OpenDoorWait4SecondsCloseYellowKeyRequired:
+            case D1_OpenDoorStayOpenYellowKeyRequired:
+                if (player->cards[it_yellowskull] == CARDNOTINMAP)
+                    player->cards[it_yellowcard] = CARDNOTFOUNDYET;
+                break;
         }
     }
 }

commit c6cd7e405747a2df5ef887d75df92e8397277dac

Fix error when initializing HUD in HacX
Caused by changes in recent commit
618751b00591dca4c4e2bc685dcef3636d3f6eee.

src/hu_stuff.c
@@ -130,11 +130,11 @@ static struct
     patch_t     *patch;
 } keypic[NUMCARDS] = {
     { "BKEYA0", NULL },
-    { "RKEYA0", NULL },
     { "YKEYA0", NULL },
+    { "RKEYA0", NULL },
+    { "BSKUA0", NULL },
     { "YSKUA0", NULL },
-    { "RSKUA0", NULL },
-    { "BSKUA0", NULL }
+    { "RSKUA0", NULL }
 };
 
 void HU_Init(void)
@@ -234,7 +234,7 @@ void HU_Start(void)
     ammopic[am_misl].patch = HU_LoadHUDAmmoPatch(am_misl);
 
     keypic[it_bluecard].patch = HU_LoadHUDKeyPatch(it_bluecard);
-    keypic[it_yellowcard].patch = HU_LoadHUDKeyPatch(it_yellowcard);
+    keypic[it_yellowcard].patch = HU_LoadHUDKeyPatch(hacx ? it_yellowskull : it_yellowcard);
     keypic[it_redcard].patch = HU_LoadHUDKeyPatch(it_redcard);
     if (gamemode != shareware)
     {