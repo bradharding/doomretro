bradharding/doomretro

commit 6dce50d7b24643005cc9467b2e02f0b4255ed337

Fix when liquid sector adjacent to lower sector
Previously, when a liquid sector was adjacent to a lower sector, there
would be a gap along that edge, which was very prevalent at eye height
if the player was on the lower sector.

src/r_segs.c
@@ -753,6 +753,10 @@ void R_StoreWallRange(int start, int stop)
         worldhigh = backsector->ceilingheight - viewz;
         worldlow = backsector->floorheight - viewz;
 
+        // [BH] animate liquid sectors
+        if (backsector->animate != INT_MAX)
+            worldlow += backsector->animate + 2 * FRACUNIT;
+
         // hack to allow height changes in outdoor areas
         if (frontsector->ceilingpic == skyflatnum && backsector->ceilingpic == skyflatnum)
             worldtop = worldhigh;

commit a7e641fbe04a08bfa5988978a0f1fef3014fac43

Specifically check if liquid sector is higher
Otherwise commit 6dce50d7b24643005cc9467b2e02f0b4255ed337 caused
problems with two adjacent liquid sectors with the same floorheight

src/r_segs.c
@@ -754,7 +754,7 @@ void R_StoreWallRange(int start, int stop)
         worldlow = backsector->floorheight - viewz;
 
         // [BH] animate liquid sectors
-        if (backsector->animate != INT_MAX)
+        if (backsector->animate != INT_MAX && backsector->floorheight > frontsector->floorheight)
             worldlow += backsector->animate + 2 * FRACUNIT;
 
         // hack to allow height changes in outdoor areas