3070d46f9f1ffb7a91f63149e9f4ea76936d7517
Brad Harding <brad@doomretro.com>
2015-04-03 10:25:30 +1100

    Fix weapon raise at map start due to interpolation

--- src/r_things.c
+++ src/r_things.c
@@ -1092,7 +1092,7 @@ static void R_DrawPSprite(pspdef_t *psp, boolean invisibility)
         psp_inter.x1_prev = vis->x1;
         psp_inter.texturemid_prev = vis->texturemid;
 
-        if (lump == psp_inter.lump)
+        if (lump == psp_inter.lump && psp->sy >= WEAPONBOTTOM)
         {
             int deltax = vis->x2 - vis->x1;
 
