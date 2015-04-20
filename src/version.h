5c5fccec5888cb561d97b10e5e8c5514962d0320
Brad Harding <brad@doomretro.com>
2015-04-20 14:59:04 +1000

    Bump version for next release
    Point release v1.7.1 will likely have fixes for whatever bugs are found
    in v1.7, as well as further functionality to the console.

--- src/version.h
+++ src/version.h
@@ -36,9 +36,9 @@
 ========================================================================
 */
 
-#define PACKAGE_VERSION                 1,7,0,0
-#define PACKAGE_VERSIONSTRING           "1.7"
-#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM RETRO v1.7"
+#define PACKAGE_VERSION                 1,7,1,0
+#define PACKAGE_VERSIONSTRING           "1.7.1"
+#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM RETRO v1.7.1"
 
 #define PACKAGE                         "doomretro"
 #define PACKAGE_CONFIG                  "doomretro.cfg"
