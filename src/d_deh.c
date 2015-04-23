a0d65b04959a6329ddcc35ed1ca0e92bd0f002a0
Brad Harding <brad@doomretro.com>
2015-04-23 16:43:53 +1000

    Don't clear flags2  and blood properties in BTSX

--- src/d_deh.c
+++ src/d_deh.c
@@ -2008,8 +2008,11 @@ void deh_procThing(DEHFILE *fpin, FILE* fpout, char *line)
                 }
                 pix = (int *)&mobjinfo[indexnum];
                 pix[ix] = (int)value;
-                mobjinfo[indexnum].flags2 = 0;
-                mobjinfo[indexnum].blood = 0;
+                if (!BTSX)
+                {
+                    mobjinfo[indexnum].flags2 = 0;
+                    mobjinfo[indexnum].blood = 0;
+                }
                 if (devparm)
                     C_Output("Assigned %d to %s(%d) at index %d", (int)value, key, indexnum, ix);
             }
