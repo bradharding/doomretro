bradharding/doomretro

commit 7947dae5d4507a6cc2f29f51538b6a6134f89074

Tweak translucent muzzle flash on chaingun

src/i_tinttab.c
@@ -45,16 +45,17 @@
 #define W               2
 #define G               4
 #define B               8
+#define X              16
 
 static byte cfilter[256] =
 {
-    0,0,0,0,R|B,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,
+    0,X,0,0,R|B,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,
     R,R,R,R,R,R,R,R,R,R,R,R,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,X,X,X,R,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,G,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,
     R,R,R,R,R,R,R,R,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,B,R,R,R,R,R,R,R,R,R,R,R,R,R,
-    R,R,R,R|B,R|B,R,R,R,R,R,R,0,0,0,0,0,0,0,0,B,B,B,B,B,B,B,B,R,R,0,0,0,0,0,0
+    R,R,R,R|B,R|B,R,R,R,R,R,R,X,X,X,X,0,0,0,0,B,B,B,B,B,B,B,B,R,R,0,0,0,0,0,0
 };
 
 #define ALL             0
@@ -62,6 +63,7 @@ static byte cfilter[256] =
 #define WHITES          W
 #define GREENS          G
 #define BLUES           B
+#define EXTRAS          X
 
 byte    *tinttab;
 
@@ -179,12 +181,12 @@ void I_InitTintTables(byte *palette)
     tinttab80 = GenerateTintTable(palette, 80, ALL);
 
     tinttabred = GenerateTintTable(palette, ADDITIVE, REDS);
-    tinttabredwhite = GenerateTintTable(palette, ADDITIVE, REDS | WHITES);
+    tinttabredwhite = GenerateTintTable(palette, ADDITIVE, (REDS | WHITES | EXTRAS));
     tinttabgreen = GenerateTintTable(palette, ADDITIVE, GREENS);
     tinttabblue = GenerateTintTable(palette, ADDITIVE, BLUES);
 
     tinttabred50 = GenerateTintTable(palette, 50, REDS);
-    tinttabredwhite50 = GenerateTintTable(palette, 50, REDS | WHITES);
+    tinttabredwhite50 = GenerateTintTable(palette, 50, (REDS | WHITES));
     tinttabgreen50 = GenerateTintTable(palette, 50, GREENS);
     tinttabblue50 = GenerateTintTable(palette, 50, BLUES);
 }

src/r_things.c
@@ -1187,7 +1187,7 @@ static void R_DrawPSprite(pspdef_t *psp, boolean invisibility)
                 /* SPR_SHTF */ tlcolfunc,
                 /* SPR_SHT2 */ tlredwhitecolfunc,
                 /* SPR_CHGG */ basecolfunc,
-                /* SPR_CHGF */ tlcolfunc,
+                /* SPR_CHGF */ tlredwhitecolfunc,
                 /* SPR_MISG */ basecolfunc,
                 /* SPR_MISF */ tlcolfunc,
                 /* SPR_SAWG */ basecolfunc,

commit 9e03b2bf08ac464ac3c6fc6d9e8989295f832690

Fix rendering of super shotgun
Commit 7947dae5d4507a6cc2f29f51538b6a6134f89074 caused some of the super
shotgun to become translucent.

src/i_tinttab.c
@@ -47,7 +47,7 @@
 #define B               8
 #define X              16
 
-static byte cfilter[256] =
+static byte general[256] =
 {
     0,X,0,0,R|B,0,0,0,0,0,0,0,0,0,0,0,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,R,
     R,R,R,R,R,R,R,R,R,R,R,R,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,W,0,0,0,0,0,0,0,0,0,
@@ -58,12 +58,26 @@ static byte cfilter[256] =
     R,R,R,R|B,R|B,R,R,R,R,R,R,X,X,X,X,0,0,0,0,B,B,B,B,B,B,B,B,R,R,0,0,0,0,0,0
 };
 
+static byte CHGF[256] =
+{
+    0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0
+};
+
 #define ALL             0
 #define REDS            R
 #define WHITES          W
 #define GREENS          G
 #define BLUES           B
 #define EXTRAS          X
+
+byte    *tranmap_solid;
 
 byte    *tinttab;
 
@@ -77,7 +91,8 @@ byte    *tinttab75;
 byte    *tinttab80;
 
 byte    *tinttabred;
-byte    *tinttabredwhite;
+byte    *tinttabredwhite1;
+byte    *tinttabredwhite2;
 byte    *tinttabgreen;
 byte    *tinttabblue;
 
@@ -85,6 +100,8 @@ byte    *tinttabred50;
 byte    *tinttabredwhite50;
 byte    *tinttabgreen50;
 byte    *tinttabblue50;
+
+byte    *tinttabchgf;
 
 int FindNearestColor(byte *palette, int red, int green, int blue)
 {
@@ -111,14 +128,14 @@ int FindNearestColor(byte *palette, int red, int green, int blue)
     return best_color;
 }
 
-static byte *GenerateTintTable(byte *palette, int percent, int colors)
+static byte *GenerateTintTable(byte *palette, int percent, byte filter[256], int colors)
 {
     byte        *result = Z_Malloc(65536, PU_STATIC, NULL);
     int         foreground, background;
 
     for (foreground = 0; foreground < 256; ++foreground)
     {
-        if ((cfilter[foreground] & colors) || colors == ALL)
+        if ((filter[foreground] & colors) || colors == ALL)
         {
             for (background = 0; background < 256; ++background)
             {
@@ -128,7 +145,7 @@ static byte *GenerateTintTable(byte *palette, int percent, int colors)
 
                 if (percent == ADDITIVE)
                 {
-                    if ((cfilter[background] & BLUES) && !(cfilter[foreground] & WHITES))
+                    if ((filter[background] & BLUES) && !(filter[foreground] & WHITES))
                     {
                         r = ((int)color1[0] * 25 + (int)color2[0] * 75) / 100;
                         g = ((int)color1[1] * 25 + (int)color2[1] * 75) / 100;
@@ -154,10 +171,8 @@ static byte *GenerateTintTable(byte *palette, int percent, int colors)
             }
         }
         else
-        {
             for (background = 0; background < 256; ++background)
                 *(result + (background << 8) + foreground) = foreground;
-        }
     }
     if (colors == ALL && percent != ADDITIVE)
     {
@@ -169,24 +184,25 @@ static byte *GenerateTintTable(byte *palette, int percent, int colors)
 
 void I_InitTintTables(byte *palette)
 {
-    tinttab = GenerateTintTable(palette, ADDITIVE, ALL);
+    tinttab = GenerateTintTable(palette, ADDITIVE, general, ALL);
 
-    tinttab25 = GenerateTintTable(palette, 25, ALL);
-    tinttab33 = GenerateTintTable(palette, 33, ALL);
-    tinttab40 = GenerateTintTable(palette, 40, ALL);
-    tinttab50 = GenerateTintTable(palette, 50, ALL);
-    tinttab60 = GenerateTintTable(palette, 60, ALL);
-    tinttab66 = GenerateTintTable(palette, 66, ALL);
-    tinttab75 = GenerateTintTable(palette, 75, ALL);
-    tinttab80 = GenerateTintTable(palette, 80, ALL);
+    tinttab25 = GenerateTintTable(palette, 25, general, ALL);
+    tinttab33 = GenerateTintTable(palette, 33, general, ALL);
+    tinttab40 = GenerateTintTable(palette, 40, general, ALL);
+    tinttab50 = GenerateTintTable(palette, 50, general, ALL);
+    tinttab60 = GenerateTintTable(palette, 60, general, ALL);
+    tinttab66 = GenerateTintTable(palette, 66, general, ALL);
+    tinttab75 = GenerateTintTable(palette, 75, general, ALL);
+    tinttab80 = GenerateTintTable(palette, 80, general, ALL);
 
-    tinttabred = GenerateTintTable(palette, ADDITIVE, REDS);
-    tinttabredwhite = GenerateTintTable(palette, ADDITIVE, (REDS | WHITES | EXTRAS));
-    tinttabgreen = GenerateTintTable(palette, ADDITIVE, GREENS);
-    tinttabblue = GenerateTintTable(palette, ADDITIVE, BLUES);
+    tinttabred = GenerateTintTable(palette, ADDITIVE, general, REDS);
+    tinttabredwhite1 = GenerateTintTable(palette, ADDITIVE, general, (REDS | WHITES));
+    tinttabredwhite2 = GenerateTintTable(palette, ADDITIVE, general, (REDS | WHITES | EXTRAS));
+    tinttabgreen = GenerateTintTable(palette, ADDITIVE, general, GREENS);
+    tinttabblue = GenerateTintTable(palette, ADDITIVE, general, BLUES);
 
-    tinttabred50 = GenerateTintTable(palette, 50, REDS);
-    tinttabredwhite50 = GenerateTintTable(palette, 50, (REDS | WHITES));
-    tinttabgreen50 = GenerateTintTable(palette, 50, GREENS);
-    tinttabblue50 = GenerateTintTable(palette, 50, BLUES);
+    tinttabred50 = GenerateTintTable(palette, 50, general, REDS);
+    tinttabredwhite50 = GenerateTintTable(palette, 50, general, (REDS | WHITES));
+    tinttabgreen50 = GenerateTintTable(palette, 50, general, GREENS);
+    tinttabblue50 = GenerateTintTable(palette, 50, general, BLUES);
 }

src/r_draw.c
@@ -554,11 +554,11 @@ void R_DrawSuperShotgunColumn(void)
         byte    dot = source[frac >> FRACBITS];
 
         if (dot != 71)
-            *dest = colormap[tinttabredwhite[(*dest << 8) + source[frac >> FRACBITS]]];
+            *dest = colormap[tinttabredwhite1[(*dest << 8) + source[frac >> FRACBITS]]];
         dest += SCREENWIDTH;
         frac += fracstep;
     }
-    *dest = colormap[tinttabredwhite[(*dest << 8) + source[frac >> FRACBITS]]];
+    *dest = colormap[tinttabredwhite1[(*dest << 8) + source[frac >> FRACBITS]]];
 }
 
 void R_DrawSkyColumn(void)
@@ -868,7 +868,7 @@ void R_DrawTranslucentRedColumn(void)
     *dest = tinttabred[(*dest << 8) + colormap[source[frac >> FRACBITS]]];
 }
 
-void R_DrawTranslucentRedWhiteColumn(void)
+void R_DrawTranslucentRedWhiteColumn1(void)
 {
     int32_t             count = dc_yh - dc_yl + 1;
     byte                *dest = R_ADDRESS(0, dc_x, dc_yl);
@@ -879,11 +879,29 @@ void R_DrawTranslucentRedWhiteColumn(void)
     
     while (--count)
     {
-        *dest = colormap[tinttabredwhite[(*dest << 8) + source[frac >> FRACBITS]]];
+        *dest = colormap[tinttabredwhite1[(*dest << 8) + source[frac >> FRACBITS]]];
         dest += SCREENWIDTH;
         frac += fracstep;
     }
-    *dest = colormap[tinttabredwhite[(*dest << 8) + source[frac >> FRACBITS]]];
+    *dest = colormap[tinttabredwhite1[(*dest << 8) + source[frac >> FRACBITS]]];
+}
+
+void R_DrawTranslucentRedWhiteColumn2(void)
+{
+    int32_t             count = dc_yh - dc_yl + 1;
+    byte                *dest = R_ADDRESS(0, dc_x, dc_yl);
+    fixed_t             frac = dc_texturefrac;
+    const fixed_t       fracstep = dc_iscale;
+    const byte          *source = dc_source;
+    const lighttable_t  *colormap = dc_colormap;
+
+    while (--count)
+    {
+        *dest = colormap[tinttabredwhite2[(*dest << 8) + source[frac >> FRACBITS]]];
+        dest += SCREENWIDTH;
+        frac += fracstep;
+    }
+    *dest = colormap[tinttabredwhite2[(*dest << 8) + source[frac >> FRACBITS]]];
 }
 
 void R_DrawTranslucentRedWhite50Column(void)

src/r_draw.h
@@ -54,10 +54,13 @@ extern boolean          dc_topsparkle;
 extern boolean          dc_bottomsparkle;
 extern byte             *dc_blood;
 extern byte             *dc_colormask;
+extern byte             *dc_tranmap;
 extern int              dc_baseclip;
 
 // first pixel in a column
 extern byte             *dc_source;
+
+extern byte             *tranmap_solid;
 
 extern byte             *tinttab;
 extern byte             *tinttab25;
@@ -66,13 +69,22 @@ extern byte             *tinttab40;
 extern byte             *tinttab50;
 extern byte             *tinttab66;
 extern byte             *tinttabred;
-extern byte             *tinttabredwhite;
+extern byte             *tinttabredwhite1;
+extern byte             *tinttabredwhite2;
 extern byte             *tinttabgreen;
 extern byte             *tinttabblue;
 extern byte             *tinttabred50;
 extern byte             *tinttabredwhite50;
 extern byte             *tinttabgreen50;
 extern byte             *tinttabblue50;
+
+extern byte             *tranmap_pistol;
+extern byte             *tranmap_shotgun;
+extern byte             *tranmap_supershotgun;
+extern byte             *tranmap_chaingun;
+extern byte             *tranmap_rocketlauncher;
+extern byte             *tranmap_plasmarifle;
+extern byte             *tranmap_bfg9000;
 
 // The span blitting interface.
 // Hook in assembler or system specific BLT
@@ -87,7 +99,8 @@ void R_DrawTranslucent50Column(void);
 void R_DrawTranslucent33Column(void);
 void R_DrawTranslucentGreenColumn(void);
 void R_DrawTranslucentRedColumn(void);
-void R_DrawTranslucentRedWhiteColumn(void);
+void R_DrawTranslucentRedWhiteColumn1(void);
+void R_DrawTranslucentRedWhiteColumn2(void);
 void R_DrawTranslucentRedWhite50Column(void);
 void R_DrawTranslucentBlueColumn(void);
 void R_DrawTranslucentGreen50Column(void);

src/r_main.c
@@ -134,7 +134,8 @@ void (*tl50colfunc)(void);
 void (*tl33colfunc)(void);
 void (*tlgreencolfunc)(void);
 void (*tlredcolfunc)(void);
-void (*tlredwhitecolfunc)(void);
+void (*tlredwhitecolfunc1)(void);
+void (*tlredwhitecolfunc2)(void);
 void (*tlredwhite50colfunc)(void);
 void (*tlbluecolfunc)(void);
 void (*tlgreen50colfunc)(void);
@@ -552,7 +553,8 @@ void R_InitColumnFunctions(void)
         tl33colfunc = R_DrawTranslucent33Column;
         tlgreencolfunc = R_DrawTranslucentGreenColumn;
         tlredcolfunc = R_DrawTranslucentRedColumn;
-        tlredwhitecolfunc = R_DrawTranslucentRedWhiteColumn;
+        tlredwhitecolfunc1 = R_DrawTranslucentRedWhiteColumn1;
+        tlredwhitecolfunc2 = R_DrawTranslucentRedWhiteColumn2;
         tlredwhite50colfunc = R_DrawTranslucentRedWhite50Column;
         tlbluecolfunc = R_DrawTranslucentBlueColumn;
         tlgreen50colfunc = R_DrawTranslucentGreen50Column;
@@ -570,7 +572,8 @@ void R_InitColumnFunctions(void)
         tl33colfunc = R_DrawColumn;
         tlgreencolfunc = R_DrawColumn;
         tlredcolfunc = R_DrawColumn;
-        tlredwhitecolfunc = R_DrawColumn;
+        tlredwhitecolfunc1 = R_DrawColumn;
+        tlredwhitecolfunc2 = R_DrawColumn;
         tlredwhite50colfunc = R_DrawColumn;
         tlbluecolfunc = R_DrawColumn;
         tlgreen50colfunc = R_DrawColumn;
@@ -611,7 +614,7 @@ void R_InitColumnFunctions(void)
         else if (flags2 & MF2_TRANSLUCENT_50)
             info->colfunc = tl50colfunc;
         else if (flags2 & MF2_TRANSLUCENT_REDWHITEONLY)
-            info->colfunc = tlredwhitecolfunc;
+            info->colfunc = tlredwhitecolfunc1;
         else if (flags2 & MF2_TRANSLUCENT_REDTOGREEN_33)
             info->colfunc = tlredtogreen33colfunc;
         else if (flags2 & MF2_TRANSLUCENT_REDTOBLUE_33)

src/r_main.h
@@ -109,7 +109,8 @@ extern void (*tl50colfunc)(void);
 extern void (*tl33colfunc)(void);
 extern void (*tlgreencolfunc)(void);
 extern void (*tlredcolfunc)(void);
-extern void (*tlredwhitecolfunc)(void);
+extern void (*tlredwhitecolfunc1)(void);
+extern void (*tlredwhitecolfunc2)(void);
 extern void (*tlredwhite50colfunc)(void);
 extern void (*tlbluecolfunc)(void);
 extern void (*tlgreen50colfunc)(void);

src/r_things.c
@@ -468,7 +468,7 @@ void R_DrawVisSprite(vissprite_t *vis)
             colfunc = tlgreen50colfunc;
         else if (colfunc == tlbluecolfunc)
             colfunc = tlblue50colfunc;
-        else if (colfunc == tlredwhitecolfunc)
+        else if (colfunc == tlredwhitecolfunc1 || colfunc == tlredwhitecolfunc2)
             colfunc = tlredwhite50colfunc;
     }
 
@@ -1185,9 +1185,9 @@ static void R_DrawPSprite(pspdef_t *psp, boolean invisibility)
                 /* SPR_PISG */ basecolfunc,
                 /* SPR_PISF */ tlcolfunc,
                 /* SPR_SHTF */ tlcolfunc,
-                /* SPR_SHT2 */ tlredwhitecolfunc,
+                /* SPR_SHT2 */ tlredwhitecolfunc1,
                 /* SPR_CHGG */ basecolfunc,
-                /* SPR_CHGF */ tlredwhitecolfunc,
+                /* SPR_CHGF */ tlredwhitecolfunc2,
                 /* SPR_MISG */ basecolfunc,
                 /* SPR_MISF */ tlcolfunc,
                 /* SPR_SAWG */ basecolfunc,