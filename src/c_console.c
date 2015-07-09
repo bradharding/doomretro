2acd7203730e93c6c3a75db819639fc711429f2e
Brad Harding <brad@doomretro.com>
2015-07-09 22:21:11 +1000

    Tweak new console background effect so it scrolls properly...
    ...as the console opens and closes.

--- src/c_console.c
+++ src/c_console.c
@@ -465,7 +465,7 @@ static void C_DrawBackground(int height)
     for (i = 0; i < height; ++i)
         screens[0][i] = tinttab50[c_blurredscreen[i] + consoletintcolor];
 
-    for (i = 0; i < height; i += 3)
+    for (i = height - 1; i >= 0; i -= 3)
     {
         screens[0][i] = colormaps[0][256 * 6 + screens[0][i]];
         screens[0][i + 1] = colormaps[0][256 * 6 + screens[0][i + 1]];
