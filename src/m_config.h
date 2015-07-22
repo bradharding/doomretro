120bf33b611903a274f812d7f0636f2d7a6438ee
Brad Harding <brad@doomretro.com>
2015-07-22 16:31:37 +1000

    Increase default mouse sensitivity from 12 back to 16

--- src/m_config.h
+++ src/m_config.h
@@ -230,7 +230,7 @@
 #define MOUSENEXTWEAPON_DEFAULT                 MOUSE_WHEELDOWN
 
 #define MOUSESENSITIVITY_MIN                    0
-#define MOUSESENSITIVITY_DEFAULT                12
+#define MOUSESENSITIVITY_DEFAULT                16
 #define MOUSESENSITIVITY_MAX                    128
 
 #define MOUSESTRAFE_DEFAULT                     -1
