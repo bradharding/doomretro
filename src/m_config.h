1d9f7ab69d2d3b34cf3ef932ddff64249d79e9d1
Brad Harding <brad@doomretro.com>
2015-08-05 16:15:56 +1000

    Set default of vid_windowsize to "768x480" (16:10 aspect ratio)

--- src/m_config.h
+++ src/m_config.h
@@ -231,7 +231,7 @@ typedef enum
 #define vid_windowposition_centered             "centered"
 #define vid_windowposition_default              vid_windowposition_centered
 
-#define vid_windowsize_default                  "640x480"
+#define vid_windowsize_default                  "768x480"
 
 #define GAMEPADAUTOMAP_DEFAULT                  GAMEPAD_BACK
 #define GAMEPADAUTOMAPCLEARMARK_DEFAULT         0
