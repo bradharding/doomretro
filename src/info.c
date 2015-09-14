91b1180cfc64c802dd860279f3dd377d5dabd667
Brad Harding <brad@doomretro.com>
2015-09-14 21:09:42 +1000

    Add MF_NOBLOOD flag to Lost Souls
    So there's no blood splats spawned when they're killed using the kill
    cmd in the console.

--- src/info.c
+++ src/info.c
@@ -2223,7 +2223,7 @@ mobjinfo_t mobjinfo[NUMMOBJTYPES] =
         /* mass                 */ 50,
         /* damage               */ 3,
         /* activesound          */ sfx_dmact,
-        /* flags                */ MF_SOLID | MF_SHOOTABLE | MF_FLOAT | MF_NOGRAVITY | MF_COUNTKILL,
+        /* flags                */ MF_SOLID | MF_SHOOTABLE | MF_FLOAT | MF_NOGRAVITY | MF_COUNTKILL | MF_NOBLOOD,
         /* flags2               */ MF2_PASSMOBJ | MF2_SHADOW | MF2_TRANSLUCENT_REDONLY | MF2_NOFOOTCLIP | MF2_NOLIQUIDBOB,
         /* raisestate           */ S_NULL,
         /* frames               */ 0,
