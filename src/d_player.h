544207d08d812f7933b917c1a81d33b9270feb3b
Brad Harding <brad@doomretro.com>
2015-09-26 08:53:17 +1000

    Replace all instances of "DOOM RETRO" with "DOOM Retro"
    All 1131 of them.

--- src/d_player.h
+++ src/d_player.h
@@ -1,7 +1,7 @@
 /*
 ========================================================================
 
-                               DOOM RETRO
+                               DOOM Retro
          The classic, refined DOOM source port. For Windows PC.
 
 ========================================================================
@@ -9,28 +9,28 @@
   Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
   Copyright (C) 2013-2015 Brad Harding.
 
-  DOOM RETRO is a fork of CHOCOLATE DOOM by Simon Howard.
+  DOOM Retro is a fork of Chocolate DOOM by Simon Howard.
   For a complete list of credits, see the accompanying AUTHORS file.
 
-  This file is part of DOOM RETRO.
+  This file is part of DOOM Retro.
 
-  DOOM RETRO is free software: you can redistribute it and/or modify it
+  DOOM Retro is free software: you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
   Free Software Foundation, either version 3 of the License, or (at your
   option) any later version.
 
-  DOOM RETRO is distributed in the hope that it will be useful, but
+  DOOM Retro is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with DOOM RETRO. If not, see <http://www.gnu.org/licenses/>.
+  along with DOOM Retro. If not, see <http://www.gnu.org/licenses/>.
 
   DOOM is a registered trademark of id Software LLC, a ZeniMax Media
   company, in the US and/or other countries and is used without
   permission. All other trademarks are the property of their respective
-  holders. DOOM RETRO is in no way affiliated with nor endorsed by
+  holders. DOOM Retro is in no way affiliated with nor endorsed by
   id Software LLC.
 
 ========================================================================
@@ -112,7 +112,7 @@ typedef struct player_s
     // killough 10/98: used for realistic bobbing (i.e. not simply overall speed)
     // mo->momx and mo->momy represent true momenta experienced by player.
     // This only represents the thrust that the player applies himself.
-    // This avoids anomalies with such things as Boom ice and conveyors.
+    // This avoids anomalies with such things as BOOM ice and conveyors.
     fixed_t             momx, momy;     // killough 10/98
 
     // This is only used between levels,
--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 /*
 ========================================================================
 
-                               DOOM RETRO
+                               DOOM Retro
          The classic, refined DOOM source port. For Windows PC.
 
 ========================================================================
@@ -9,28 +9,28 @@
   Copyright (C) 1993-2012 id Software LLC, a ZeniMax Media company.
   Copyright (C) 2013-2015 Brad Harding.
 
-  DOOM RETRO is a fork of CHOCOLATE DOOM by Simon Howard.
+  DOOM Retro is a fork of Chocolate DOOM by Simon Howard.
   For a complete list of credits, see the accompanying AUTHORS file.
 
-  This file is part of DOOM RETRO.
+  This file is part of DOOM Retro.
 
-  DOOM RETRO is free software: you can redistribute it and/or modify it
+  DOOM Retro is free software: you can redistribute it and/or modify it
   under the terms of the GNU General Public License as published by the
   Free Software Foundation, either version 3 of the License, or (at your
   option) any later version.
 
-  DOOM RETRO is distributed in the hope that it will be useful, but
+  DOOM Retro is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with DOOM RETRO. If not, see <http://www.gnu.org/licenses/>.
+  along with DOOM Retro. If not, see <http://www.gnu.org/licenses/>.
 
   DOOM is a registered trademark of id Software LLC, a ZeniMax Media
   company, in the US and/or other countries and is used without
   permission. All other trademarks are the property of their respective
-  holders. DOOM RETRO is in no way affiliated with nor endorsed by
+  holders. DOOM Retro is in no way affiliated with nor endorsed by
   id Software LLC.
 
 ========================================================================
@@ -39,12 +39,12 @@
 #define PACKAGE_VERSION                 1,9,0,0
 #define PACKAGE_VERSIONSTRING           "1.9"
 #if defined (_DEBUG)
-#define PACKAGE_BRANDINGSTRING          "DOOM RETRO v1.9 (Debug Build)"
+#define PACKAGE_BRANDINGSTRING          "DOOM Retro v1.9 (Debug Build)"
 #else
-#define PACKAGE_BRANDINGSTRING          "DOOM RETRO v1.9"
+#define PACKAGE_BRANDINGSTRING          "DOOM Retro v1.9"
 #endif
-#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM RETRO v1.9"
-#define PACKAGE_SAVEGAMEVERSIONSTRING   "DOOM RETRO v1.9"
+#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v1.9"
+#define PACKAGE_SAVEGAMEVERSIONSTRING   "DOOM Retro v1.9"
 
 #define PACKAGE                         "doomretro"
 #define PACKAGE_CONFIG                  "doomretro.cfg"
@@ -52,7 +52,7 @@
 #define PACKAGE_EMAIL                   "brad@doomretro.com"
 #define PACKAGE_ICON_PATH               "..\\res\\doomretro.ico"
 #define PACKAGE_MUTEX                   "DOOMRETRO-CC4F1071-8B24-4E91-A207-D792F39636CD"
-#define PACKAGE_NAME                    "DOOM RETRO"
+#define PACKAGE_NAME                    "DOOM Retro"
 #define PACKAGE_SAVE                    "doomretro%d.save"
 #define PACKAGE_WAD                     "doomretro.wad"
 #define PACKAGE_WIKI_URL                "http://wiki.doomretro.com/Starting-a-Game"
