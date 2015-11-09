c180ee5b710bb0ca3fd153fb62d469a164ecab76
Brad Harding <brad@doomretro.com>
2015-11-09 19:27:07 +1100

    Fix typo from a previous commit

--- src/p_setup.c
+++ src/p_setup.c
@@ -2134,7 +2134,7 @@ static void InitMapInfo(void)
 
         // Map name must follow the number
         SC_MustGetString();
-        if (SC_Compare("LOOKUP"))
+        if (!SC_Compare("LOOKUP"))
             M_StringCopy(info->name, sc_String, sizeof(info->name));
 
         // Process optional tokens
