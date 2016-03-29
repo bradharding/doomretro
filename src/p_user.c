548c66106befacf3ceae24d639c26360abcf9cfb
Brad Harding <brad@doomretro.com>
2016-03-29 18:26:46 +1100

    No need for player->powers[pw_strength] to count up

--- src/p_user.c
+++ src/p_user.c
@@ -467,10 +467,6 @@ void P_PlayerThink(player_t *player)
 
     // Counters, time dependent power ups.
 
-    // Strength counts up to diminish fade.
-    if (player->powers[pw_strength])
-        player->powers[pw_strength]++;
-
     if (player->powers[pw_invulnerability] > 0)
         player->powers[pw_invulnerability]--;
 
