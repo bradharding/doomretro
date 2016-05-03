bac308c38649997f812610dd4722c65b150007b1
Brad Harding <brad@doomretro.com>
2016-05-03 13:20:05 +1000

    Minor tweak to r_floatbob CVAR description

--- src/c_cmds.c
+++ src/c_cmds.c
@@ -538,7 +538,7 @@ consolecmd_t consolecmds[] =
     CVAR_BOOL (r_diskicon, "", bool_cvars_func1, bool_cvars_func2, "Toggles showing a disk icon when loading and saving."),
     CVAR_BOOL (r_fixmaperrors, "", bool_cvars_func1, bool_cvars_func2, "Toggles the fixing of mapping errors in the ~DOOM~ and ~DOOM II~\nIWADs."),
     CVAR_BOOL (r_fixspriteoffsets, "", bool_cvars_func1, bool_cvars_func2, "Toggles the fixing of sprite offsets."),
-    CVAR_BOOL (r_floatbob, "", bool_cvars_func1, bool_cvars_func2, "Toggles powerups bobbing up and down."),
+    CVAR_BOOL (r_floatbob, "", bool_cvars_func1, bool_cvars_func2, "Toggles power-ups bobbing up and down."),
     CVAR_FLOAT(r_gamma, "", r_gamma_cvar_func1, r_gamma_cvar_func2, CF_NONE, "The gamma correction level (off or 0.50 to 2.0)."),
     CVAR_BOOL (r_homindicator, "", bool_cvars_func1, bool_cvars_func2, "Toggles the flashing Hall of Mirrors indicator."),
     CVAR_BOOL (r_hud, "", bool_cvars_func1, r_hud_cvar_func2, "Toggles the heads-up display when in widescreen mode."),
