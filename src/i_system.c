92ee233735b0c4adee91b3a398842b66cc6ab7df
Brad Harding <brad@doomretro.com>
2016-07-17 11:38:52 +1000

    Italicize Windows version in console at startup

--- src/i_system.c
+++ src/i_system.c
@@ -198,7 +198,7 @@ void I_PrintWindowsVersion(void)
             else if (info.dwMajorVersion == 10 && info.dwMinorVersion == 0)
                 infoname = "10";
 
-            C_Output("Running on Microsoft Windows %s%s%s%s%ws%s%s.",
+            C_Output("Running on <i>Microsoft Windows %s%s%s%s%ws%s%s</i>.",
                 infoname, (strlen(typename) ? " " : ""), (strlen(typename) ? typename : ""),
                 (wcslen(info.szCSDVersion) ? " (" : ""),
                 (wcslen(info.szCSDVersion) ? info.szCSDVersion : L""),
