c7e76947d9b2d65fd115a45d14983e76b4dffbde
Brad Harding <brad@doomretro.com>
2016-09-01 15:41:14 +1000

    Tweaks to output of playerstats ccmd

--- src/c_cmds.c
+++ src/c_cmds.c
@@ -2706,16 +2706,16 @@ static void C_PlayerStats_Game(void)
         commify(player->itemcount), commify(totalitems),
         (totalitems ? player->itemcount * 100 / totalitems : 0), commify(stat_itemspickedup));
 
-    C_TabbedOutput(tabs, "   Ammo - Bullets\t<b>%s</b>\t<b>%s</b>",
+    C_TabbedOutput(tabs, "   Ammo\t<b>%s bullets</b>\t<b>%s bullets</b>",
         commify(player->itemspickedup_ammo_bullets), commify(stat_itemspickedup_ammo_bullets));
 
-    C_TabbedOutput(tabs, "   Ammo - Cells\t<b>%s</b>\t<b>%s</b>",
+    C_TabbedOutput(tabs, "\t<b>%s cells</b>\t<b>%s cells</b>",
         commify(player->itemspickedup_ammo_cells), commify(stat_itemspickedup_ammo_cells));
 
-    C_TabbedOutput(tabs, "   Ammo - Rockets\t<b>%s</b>\t<b>%s</b>",
+    C_TabbedOutput(tabs, "\t<b>%s rockets</b>\t<b>%s rockets</b>",
         commify(player->itemspickedup_ammo_rockets), commify(stat_itemspickedup_ammo_rockets));
 
-    C_TabbedOutput(tabs, "   Ammo - Shells\t<b>%s</b>\t<b>%s</b>",
+    C_TabbedOutput(tabs, "\t<b>%s shells</b>\t<b>%s shells</b>",
         commify(player->itemspickedup_ammo_shells), commify(stat_itemspickedup_ammo_shells));
 
     C_TabbedOutput(tabs, "   Armor\t<b>%s</b>\t<b>%s</b>",
@@ -2833,13 +2833,13 @@ static void C_PlayerStats_NoGame(void)
 
     C_TabbedOutput(tabs, "Items picked up\t<b>%s</b>", commify(stat_itemspickedup));
 
-    C_TabbedOutput(tabs, "   Ammo - Bullets\t<b>%s</b>", commify(stat_itemspickedup_ammo_bullets));
+    C_TabbedOutput(tabs, "   Ammo\t<b>%s bullets</b>", commify(stat_itemspickedup_ammo_bullets));
 
-    C_TabbedOutput(tabs, "   Ammo - Cells\t<b>%s</b>", commify(stat_itemspickedup_ammo_cells));
+    C_TabbedOutput(tabs, "\t<b>%s cells</b>", commify(stat_itemspickedup_ammo_cells));
 
-    C_TabbedOutput(tabs, "   Ammo - Rockets\t<b>%s</b>", commify(stat_itemspickedup_ammo_rockets));
+    C_TabbedOutput(tabs, "\t<b>%s rockets</b>", commify(stat_itemspickedup_ammo_rockets));
 
-    C_TabbedOutput(tabs, "   Ammo - Shells\t<b>%s</b>", commify(stat_itemspickedup_ammo_shells));
+    C_TabbedOutput(tabs, "\t<b>%s shells</b>", commify(stat_itemspickedup_ammo_shells));
 
     C_TabbedOutput(tabs, "   Armor\t<b>%s</b>", commify(stat_itemspickedup_armor));
 
