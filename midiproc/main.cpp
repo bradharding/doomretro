90bc4bc1ea31df18d0471dbc7b81fe803006ad2f
Brad Harding <brad@doomretro.com>
2016-10-27 11:12:56 +1100

    Reformat midiproc project

--- midiproc/main.cpp
+++ midiproc/main.cpp
@@ -39,27 +39,25 @@
 #include "midiproc.h"
 
 // Currently playing music track
-static Mix_Music *music = NULL;
-static SDL_RWops *rw    = NULL;
+static Mix_Music        *music = NULL;
+static SDL_RWops        *rw = NULL;
 
 static void UnregisterSong();
 
-//=============================================================================
 //
 // RPC Memory Management
 //
 
 void __RPC_FAR * __RPC_USER midl_user_allocate(size_t size)
 {
-   return malloc(size);
+    return malloc(size);
 }
 
 void __RPC_USER midl_user_free(void __RPC_FAR *p)
 {
-   free(p);
+    free(p);
 }
 
-//=============================================================================
 //
 // SDL_mixer Interface
 //
@@ -71,13 +69,13 @@ void __RPC_USER midl_user_free(void __RPC_FAR *p)
 //
 static bool InitSDL()
 {
-   if(SDL_Init(SDL_INIT_AUDIO) == -1)
-      return false;
+    if (SDL_Init(SDL_INIT_AUDIO) == -1)
+        return false;
 
-   if(Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
-      return false;
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+        return false;
 
-   return true;
+    return true;
 }
 
 //
@@ -85,11 +83,11 @@ static bool InitSDL()
 //
 static void RegisterSong(void *data, size_t size)
 {
-   if(music)
-      UnregisterSong();
+    if (music)
+       UnregisterSong();
 
-   rw    = SDL_RWFromMem(data, size);
-   music = Mix_LoadMUS_RW(rw, SDL_FALSE);
+    rw = SDL_RWFromMem(data, size);
+    music = Mix_LoadMUS_RW(rw, SDL_FALSE);
 }
 
 //
@@ -97,8 +95,8 @@ static void RegisterSong(void *data, size_t size)
 //
 static void StartSong(bool loop)
 {
-   if(music)
-      Mix_PlayMusic(music, loop ? -1 : 0);
+    if (music)
+        Mix_PlayMusic(music, (loop ? -1 : 0));
 }
 
 //
@@ -106,18 +104,18 @@ static void StartSong(bool loop)
 //
 static void SetVolume(int volume)
 {
-   Mix_VolumeMusic(volume);
+    Mix_VolumeMusic(volume);
 }
 
-static int paused_midi_volume;
+static int      paused_midi_volume;
 
 //
 // PauseSong
 //
 static void PauseSong()
 {
-   paused_midi_volume = Mix_VolumeMusic(-1);
-   Mix_VolumeMusic(0);
+    paused_midi_volume = Mix_VolumeMusic(-1);
+    Mix_VolumeMusic(0);
 }
 
 //
@@ -125,7 +123,7 @@ static void PauseSong()
 //
 static void ResumeSong()
 {
-   Mix_VolumeMusic(paused_midi_volume);
+    Mix_VolumeMusic(paused_midi_volume);
 }
 
 //
@@ -133,8 +131,8 @@ static void ResumeSong()
 //
 static void StopSong()
 {
-   if(music)
-      Mix_HaltMusic();
+    if (music)
+        Mix_HaltMusic();
 }
 
 //
@@ -142,13 +140,13 @@ static void StopSong()
 //
 static void UnregisterSong()
 {
-   if(!music)
-      return;
+    if (!music)
+        return;
 
-   StopSong();
-   Mix_FreeMusic(music);
-   rw    = NULL;
-   music = NULL;
+    StopSong();
+    Mix_FreeMusic(music);
+    rw = NULL;
+    music = NULL;
 }
 
 //
@@ -156,78 +154,75 @@ static void UnregisterSong()
 //
 static void ShutdownSDL()
 {
-   UnregisterSong();
-   Mix_CloseAudio();
-   SDL_Quit();
+    UnregisterSong();
+    Mix_CloseAudio();
+    SDL_Quit();
 }
 
-//=============================================================================
 //
 // Song Buffer
 //
 // The MIDI program will be transmitted by the client across RPC in fixed-size
 // chunks until all data has been transmitted.
 //
-
-typedef unsigned char midibyte;
+typedef unsigned char   midibyte;
 
 class SongBuffer
 {
 protected:
-   midibyte *buffer;    // accumulated input
-   size_t    size;      // size of input
-   size_t    allocated; // amount of memory allocated (>= size)
+    midibyte            *buffer;                // accumulated input
+    size_t              size;                   // size of input
+    size_t              allocated;              // amount of memory allocated (>= size)
 
-   static const int defaultSize = 128*1024; // 128 KB
+    static const int    defaultSize = 128*1024; // 128 KB
 
 public:
-   // Constructor
-   // Start out with an empty 128 KB buffer.
-   SongBuffer()
-   {
-      buffer = static_cast<midibyte *>(calloc(1, defaultSize));
-      size = 0;
-      allocated = defaultSize;
-   }
+    // Constructor
+    // Start out with an empty 128 KB buffer.
+    SongBuffer()
+    {
+        buffer = static_cast<midibyte *>(calloc(1, defaultSize));
+        size = 0;
+        allocated = defaultSize;
+    }
 
-   // Destructor.
-   // Release the buffer.
-   ~SongBuffer()
-   {
-      if(buffer)
-      {
-         free(buffer);
-         buffer = NULL;
-         size = allocated = 0;
-      }
-   }
+    // Destructor.
+    // Release the buffer.
+    ~SongBuffer()
+    {
+        if(buffer)
+        {
+            free(buffer);
+            buffer = NULL;
+            size = 0;
+            allocated = 0;
+        }
+    }
 
-   //
-   // addChunk
-   //
-   // Add a chunk of MIDI data to the buffer.
-   //
-   void addChunk(midibyte *data, size_t newsize)
-   {
-      if(size + newsize > allocated)
-      {
-         allocated += newsize * 2;
-         buffer = static_cast<midibyte *>(realloc(buffer, allocated));
-      }
+    //
+    // addChunk
+    //
+    // Add a chunk of MIDI data to the buffer.
+    //
+    void addChunk(midibyte *data, size_t newsize)
+    {
+        if (size + newsize > allocated)
+        {
+            allocated += newsize * 2;
+            buffer = static_cast<midibyte *>(realloc(buffer, allocated));
+        }
 
-      memcpy(buffer + size, data, newsize);
-      size += newsize;
-   }
+        memcpy(buffer + size, data, newsize);
+        size += newsize;
+    }
 
-   // Accessors
-
-   midibyte *getBuffer() const { return buffer; }
-   size_t    getSize()   const { return size;   }
+    // Accessors
+    midibyte *getBuffer() const { return buffer; }
+    size_t    getSize()   const { return size; }
 };
 
 static SongBuffer *song;
 
-//=============================================================================
 //
 // RPC Server Interface
 //
@@ -239,14 +234,14 @@ static SongBuffer *song;
 //
 void MidiRPC_PrepareNewSong()
 {
-   // Stop anything currently playing and free it.
-   UnregisterSong();
+    // Stop anything currently playing and free it.
+    UnregisterSong();
 
-   // free any previous song buffer
-   delete song;
+    // free any previous song buffer
+    delete song;
 
-   // prep new song buffer
-   song = new SongBuffer();
+    // prep new song buffer
+    song = new SongBuffer();
 }
 
 //
@@ -256,7 +251,7 @@ void MidiRPC_PrepareNewSong()
 //
 void MidiRPC_AddChunk(unsigned int count, byte *pBuf)
 {
-   song->addChunk(pBuf, static_cast<size_t>(count));
+    song->addChunk(pBuf, static_cast<size_t>(count));
 }
 
 //
@@ -266,8 +261,8 @@ void MidiRPC_AddChunk(unsigned int count, byte *pBuf)
 //
 void MidiRPC_PlaySong(boolean looping)
 {
-   RegisterSong(song->getBuffer(), song->getSize());
-   StartSong(!!looping);
+    RegisterSong(song->getBuffer(), song->getSize());
+    StartSong(!!looping);
 }
 
 //
@@ -277,7 +272,7 @@ void MidiRPC_PlaySong(boolean looping)
 //
 void MidiRPC_StopSong()
 {
-   StopSong();
+    StopSong();
 }
 
 //
@@ -297,7 +292,7 @@ void MidiRPC_ChangeVolume(int volume)
 //
 void MidiRPC_PauseSong()
 {
-   PauseSong();
+    PauseSong();
 }
 
 //
@@ -307,7 +302,7 @@ void MidiRPC_PauseSong()
 //
 void MidiRPC_ResumeSong()
 {
-   ResumeSong();
+    ResumeSong();
 }
 
 //
@@ -317,13 +312,13 @@ void MidiRPC_ResumeSong()
 //
 void MidiRPC_StopServer()
 {
-   // Local shutdown tasks
-   ShutdownSDL();
-   delete song;
-   song = NULL;
+    // Local shutdown tasks
+    ShutdownSDL();
+    delete song;
+    song = NULL;
 
-   // Stop RPC server
-   RpcMgmtStopServerListening(NULL);
+    // Stop RPC server
+    RpcMgmtStopServerListening(NULL);
 }
 
 //
@@ -331,34 +326,27 @@ void MidiRPC_StopServer()
 //
 static bool MidiRPC_InitServer()
 {
-   RPC_STATUS status;
+    RPC_STATUS  status;
 
-   // Initialize RPC protocol
-   status = 
-      RpcServerUseProtseqEp
-      (
-         (RPC_CSTR)("ncalrpc"),
-         RPC_C_PROTSEQ_MAX_REQS_DEFAULT,
-         (RPC_CSTR)("2d4dc2f9-ce90-4080-8a00-1cb819086970"),
-         NULL
-      );
+    // Initialize RPC protocol
+    status = RpcServerUseProtseqEp((RPC_CSTR)"ncalrpc", RPC_C_PROTSEQ_MAX_REQS_DEFAULT,
+        (RPC_CSTR)"2d4dc2f9-ce90-4080-8a00-1cb819086970", NULL);
 
-   if(status)
-      return false;
+    if (status)
+        return false;
 
-   // Register server
-   status = RpcServerRegisterIf(MidiRPC_v1_0_s_ifspec, NULL, NULL);
+    // Register server
+    status = RpcServerRegisterIf(MidiRPC_v1_0_s_ifspec, NULL, NULL);
 
-   if(status)
-      return false;
+    if (status)
+        return false;
 
-   // Start listening
-   status = RpcServerListen(1, RPC_C_LISTEN_MAX_CALLS_DEFAULT, FALSE);
+    // Start listening
+    status = RpcServerListen(1, RPC_C_LISTEN_MAX_CALLS_DEFAULT, FALSE);
 
-   return !status;
+    return !status;
 }
 
-//=============================================================================
 //
 // Main Program
 //
@@ -368,19 +356,15 @@ static bool MidiRPC_InitServer()
 //
 // Application entry point.
 //
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
-                     LPSTR lpCmdLine, int nCmdShow)
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-   // Initialize SDL
-   if(!InitSDL())
-      return -1;
+    // Initialize SDL
+    if (!InitSDL())
+        return -1;
 
-   // Initialize RPC Server
-   if(!MidiRPC_InitServer())
-      return -1;
+    // Initialize RPC Server
+    if (!MidiRPC_InitServer())
+        return -1;
 
-   return 0;
+    return 0;
 }
-
-// EOF
-
