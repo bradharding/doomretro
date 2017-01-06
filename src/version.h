de050f5df3174fb796c11423223d3c5cff743218
Brad Harding <brad@doomretro.com>
2017-01-06 17:05:28 +1100

    Bump version for next release

--- src/version.h
+++ src/version.h
@@ -39,9 +39,9 @@
 #if !defined(__VERSION_H__)
 #define __VERSION_H__
 
-#define PACKAGE_VERSION                 2,3,8,0
-#define PACKAGE_VERSIONSTRING           "2.3.8"
-#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v2.3.8"
+#define PACKAGE_VERSION                 2,3,9,0
+#define PACKAGE_VERSIONSTRING           "2.3.9"
+#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v2.3.9"
 #define PACKAGE_SAVEGAMEVERSIONSTRING   "DOOM Retro v2.3.6"
 
 #define PACKAGE                         "doomretro"
