ad23463c899c58b24dc381900c890083b3fbe556
Brad Harding <brad@doomretro.com>
2017-02-15 17:33:43 +1100

    Still draw voodoo dolls

--- src/r_things.c
+++ src/r_things.c
@@ -693,7 +693,7 @@ void R_ProjectSprite(mobj_t *thing)
     fixed_t             offset;
     fixed_t             topoffset;
 
-    if (flags2 & MF2_DONTDRAW)
+    if ((flags2 & MF2_DONTDRAW) && thing->player && thing->player->mo == thing)
         return;
 
     // [AM] Interpolate between current and last position, if prudent.
