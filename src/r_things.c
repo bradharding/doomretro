72c870d4f15cb5803ae77b5e1e27f8001ebd4cde
Brad Harding <brad@doomretro.com>
2017-02-15 19:38:32 +1100

    No need to check for masked textures when clipping bloodsplats

--- src/r_things.c
+++ src/r_things.c
@@ -1238,7 +1238,7 @@ static void R_DrawBloodSplatSprite(vissprite_t *spr)
         int     silhouette = ds->silhouette;
 
         // determine if the drawseg obscures the sprite
-        if (ds->x1 > x2 || ds->x2 < x1 || (!(silhouette & SIL_BOTH) && !ds->maskedtexturecol))
+        if (ds->x1 > x2 || ds->x2 < x1 || !(silhouette & SIL_BOTH))
             continue;       // does not cover sprite
 
         if (MAX(ds->scale1, ds->scale2) < spr->scale
