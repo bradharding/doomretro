bradharding/doomretro

commit da3409fbfb1a426e13d90ab34784535cb523dd56

Minor tweaks

src/d_iwad.c
@@ -531,14 +531,12 @@ void D_SetSaveGameFolder(void)
     {
         M_MakeDirectory(appdatafolder);
 
-        savegamefolder = M_StringJoin(appdatafolder, DIR_SEPARATOR_S, "savegames",
-            DIR_SEPARATOR_S, NULL);
+        savegamefolder = M_StringJoin(appdatafolder, DIR_SEPARATOR_S, "savegames", DIR_SEPARATOR_S, NULL);
     }
 
     M_MakeDirectory(savegamefolder);
 
-    savegamefolder = M_StringJoin(savegamefolder, (pwadfile[0] ? pwadfile : iwad_name),
-        DIR_SEPARATOR_S, NULL);
+    savegamefolder = M_StringJoin(savegamefolder, (pwadfile ? pwadfile : iwad_name), DIR_SEPARATOR_S, NULL);
     M_MakeDirectory(savegamefolder);
 
     C_Output("Savegames will be saved and loaded in <b>%s</b>.", savegamefolder);

src/i_video.c
@@ -1382,8 +1382,8 @@ static void SetVideoMode(dboolean output)
 
             if (output)
                 C_Output("Staying at the desktop resolution of %s\xD7%s%s%s%s with a %s aspect ratio.",
-                    commify(width), commify(height), (acronym[0] ? " (" : " "), acronym,
-                    (acronym[0] ? ")" : ""), ratio);
+                    commify(width), commify(height), (acronym ? " (" : " "), acronym, (acronym ? ")" : ""),
+                    ratio);
         }
         else
         {
@@ -1402,8 +1402,8 @@ static void SetVideoMode(dboolean output)
 
             if (output)
                 C_Output("Switched to a resolution of %s\xD7%s%s%s%s with a %s aspect ratio.",
-                    commify(width), commify(height), (acronym[0] ? " (" : " "), acronym,
-                    (acronym[0] ? ")" : ""), ratio);
+                    commify(width), commify(height), (acronym ? " (" : " "), acronym, (acronym ? ")" : ""),
+                    ratio);
         }
     }
     else
@@ -1652,11 +1652,10 @@ static void SetVideoMode(dboolean output)
         I_SDLError("SDL_FillRect");
 
     if (nearestlinear)
-        SDL_SetHintWithPriority(SDL_HINT_RENDER_SCALE_QUALITY, vid_scalefilter_nearest,
-            SDL_HINT_OVERRIDE);
+        SDL_SetHintWithPriority(SDL_HINT_RENDER_SCALE_QUALITY, vid_scalefilter_nearest, SDL_HINT_OVERRIDE);
 
-    if (!(texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
-        SDL_TEXTUREACCESS_STREAMING, SCREENWIDTH, SCREENHEIGHT)))
+    if (!(texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
+        SCREENWIDTH, SCREENHEIGHT)))
         I_SDLError("SDL_CreateTexture");
 
     if (nearestlinear)

src/p_enemy.c
@@ -2084,8 +2084,7 @@ void A_SpawnFly(mobj_t *actor, player_t *player, pspdef_t *psp)
 
         // Will the next move put the cube closer to
         // the target point than it is now?
-        dist = P_ApproxDistance(targ->x - (actor->x + actor->momx),
-            targ->y - (actor->y + actor->momy));
+        dist = P_ApproxDistance(targ->x - (actor->x + actor->momx), targ->y - (actor->y + actor->momy));
         if ((unsigned int)dist < (unsigned int)actor->reactiontime)
         {
             actor->reactiontime = dist; // Yes. Still flying

src/p_setup.c
@@ -2538,7 +2538,7 @@ static int QualifyMap(int map)
 
 char *P_GetMapAuthor(int map)
 {
-    return (MAPINFO >= 0 && mapinfo[QualifyMap(map)].author[0] ? mapinfo[QualifyMap(map)].author
+    return (MAPINFO >= 0 && mapinfo[QualifyMap(map)].author ? mapinfo[QualifyMap(map)].author
         : (breach && map == 1 ? s_AUTHOR_BESTOR : ((E1M4B && map == 4) || (E1M8B && map == 8)
         ? s_AUTHOR_ROMERO : "")));
 }

src/r_patch.c
@@ -564,43 +564,38 @@ static void createTextureCompositePatch(int id)
         }
     }
 
+    // copy the patch image down and to the right where there are
+    // holes to eliminate the black halo from bilinear filtering
+    for (x = 0; x < composite_patch->width; x++)
     {
-        const rcolumn_t *column;
-        const rcolumn_t *prevColumn;
+        const rcolumn_t *column = R_GetPatchColumnClamped(composite_patch, x);
+        const rcolumn_t *prevColumn = R_GetPatchColumnClamped(composite_patch, x - 1);
 
-        // copy the patch image down and to the right where there are
-        // holes to eliminate the black halo from bilinear filtering
-        for (x = 0; x < composite_patch->width; x++)
+        if (column->pixels[0] == 0xFF)
         {
-            column = R_GetPatchColumnClamped(composite_patch, x);
-            prevColumn = R_GetPatchColumnClamped(composite_patch, x - 1);
-
-            if (column->pixels[0] == 0xFF)
-            {
-                // force the first pixel (which is a hole), to use
-                // the color from the next solid spot in the column
-                for (y = 0; y < composite_patch->height; y++)
-                {
-                    if (column->pixels[y] != 0xFF)
-                    {
-                        column->pixels[0] = column->pixels[y];
-                        break;
-                    }
-                }
-            }
-
-            // copy from above or to the left
-            for (y = 1; y < composite_patch->height; y++)
+            // force the first pixel (which is a hole), to use
+            // the color from the next solid spot in the column
+            for (y = 0; y < composite_patch->height; y++)
             {
                 if (column->pixels[y] != 0xFF)
-                    continue;
-
-                // this pixel is a hole
-                if (x && prevColumn->pixels[y - 1] != 0xFF)
-                    column->pixels[y] = prevColumn->pixels[y];  // copy the color from the left
-                else
-                    column->pixels[y] = column->pixels[y - 1];  // copy the color from above
+                {
+                    column->pixels[0] = column->pixels[y];
+                    break;
+                }
             }
+        }
+
+        // copy from above or to the left
+        for (y = 1; y < composite_patch->height; y++)
+        {
+            if (column->pixels[y] != 0xFF)
+                continue;
+
+            // this pixel is a hole
+            if (x && prevColumn->pixels[y - 1] != 0xFF)
+                column->pixels[y] = prevColumn->pixels[y];      // copy the color from the left
+            else
+                column->pixels[y] = column->pixels[y - 1];      // copy the color from above
         }
     }
 

commit ac0bb0b96e1f8f60c40cac6ce332251490c99a66

Fix IWAD savegames folder
Caused due to tweak in commit da3409fbfb1a426e13d90ab34784535cb523dd56. This is why it looked like there was an extra slash at the end, so I've brought that back.

src/d_iwad.c
@@ -536,7 +536,7 @@ void D_SetSaveGameFolder(void)
 
     M_MakeDirectory(savegamefolder);
 
-    savegamefolder = M_StringJoin(savegamefolder, (pwadfile ? pwadfile : iwad_name), NULL);
+    savegamefolder = M_StringJoin(savegamefolder, (*pwadfile ? pwadfile : iwad_name), DIR_SEPARATOR_S, NULL);
     M_MakeDirectory(savegamefolder);
 
     C_Output("Savegames will be saved and loaded in <b>%s</b>.", savegamefolder);