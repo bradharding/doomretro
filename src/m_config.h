37f7827d16d24b72823d3249b659dcef7f5f7be3
Brad Harding <brad@doomretro.com>
2017-05-21 09:30:25 +1000

    Double r_bloodsplats_max cvar default to 65536

--- src/m_config.h
+++ src/m_config.h
@@ -237,7 +237,7 @@ typedef enum
 #define r_blood_max                             r_blood_all
 
 #define r_bloodsplats_max_min                   0
-#define r_bloodsplats_max_default               32768
+#define r_bloodsplats_max_default               65536
 #define r_bloodsplats_max_max                   1048576
 
 #define r_bloodsplats_total_min                 0
