4f17bfa1815b512b4bb925c1be760396c868f7d2
Brad Harding <brad@doomretro.com>
2017-06-11 11:04:56 +1000

    Stretch skies better when mouselook on
    Plus a bit of refactoring.

--- src/r_sky.c
+++ src/r_sky.c
@@ -104,29 +104,15 @@ void R_InitSkyMap(void)
     {
         int skyheight = textureheight[skytexture] >> FRACBITS;
 
-        // There are various combinations for sky rendering depending on how tall the sky is:
-        //        h <  128: Unstretched and tiled, centered on horizon
-        // 128 <= h <  200: Can possibly be stretched. When unstretched, the baseline is
-        //                  28 rows below the horizon so that the top of the texture
-        //                  aligns with the top of the screen when looking straight ahead.
-        //                  When stretched, it is scaled to 228 pixels with the baseline
-        //                  in the same location as an unstretched 128-tall sky, so the top
-        //                  of the texture aligns with the top of the screen when looking
-        //                  fully up.
-        //        h == 200: Unstretched, baseline is on horizon, and top is at the top of
-        //                  the screen when looking fully up.
-        //        h >  200: Unstretched, but the baseline is shifted down so that the top
-        //                  of the texture is at the top of the screen when looking fully up.
-        skytexturemid = 0;
-
         if (skyheight >= 128 && skyheight < 200)
-            skytexturemid = -28 * FRACUNIT;
+            skytexturemid = -28 * FRACUNIT * 2 * skyheight / SKYSTRETCH_HEIGHT;
         else if (skyheight > 200)
-            skytexturemid = (200 - skyheight) << FRACBITS;
+            skytexturemid = ((200 - skyheight) << FRACBITS) * skyheight / SKYSTRETCH_HEIGHT;
+        else
+            skytexturemid = 0;
 
-        skyiscale = (fixed_t)(((uint64_t)FRACUNIT * SCREENWIDTH * 200) / (viewwidth * SCREENHEIGHT));
-        skyiscale = skyiscale * skyheight / SKYSTRETCH_HEIGHT;
-        skytexturemid = skytexturemid * skyheight / SKYSTRETCH_HEIGHT;
+        skyiscale = (fixed_t)(((uint64_t)FRACUNIT * SCREENWIDTH * 200) / (viewwidth * SCREENHEIGHT))
+            * skyheight / SKYSTRETCH_HEIGHT;
     }
     else
     {
