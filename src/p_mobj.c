270168bef44ba377540fb1a80b4b3c90b414cb15
Brad Harding <brad@doomretro.com>
2017-06-18 11:47:16 +1000

    Restore Lost Soul target amnesia
    Although it is a bug, fixing it changes the behavior of Lost Souls too much. See https://doomwiki.org/wiki/Lost_soul_target_amnesia and issue #352.

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -190,7 +190,7 @@ void P_XYMovement(mobj_t *mo)
             // the skull slammed into something
             mo->flags &= ~MF_SKULLFLY;
             mo->momz = 0;
-            P_SetMobjState(mo, mo->info->seestate);
+            P_SetMobjState(mo, mo->info->spawnstate);
         }
 
         return;
