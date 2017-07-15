bradharding/doomretro

commit 80f48e5d15d4a9e420c8205331f23a34b8eea32f

Don't change player weapon offset if changed
When merging sprites from a PWAD, if there are any player weapon sprites, use the default x,y offset for every sprite for that weapon. Fixes the puppy weapon from Doomguy Gets A Puppy (https://www.doomworld.com/forum/topic/94499-doomguy-gets-a-puppy/).

src/w_merge.c
@@ -263,6 +263,15 @@ static dboolean SpriteLumpNeeded(lumpinfo_t *lump)
     return false;
 }
 
+struct
+{
+    char    *spr1;
+    char    *spr2;
+} weaponsprites[] = {
+    { "PUNG", ""     }, { "PISG", "PISF" }, { "SHTG", "SHTF" }, { "CHGG", "CHGF" }, { "MISG", "MISF" },
+    { "PLSG", "PLSF" }, { "BFGG", "BFGF" }, { "SAWG", ""     }, { "SHT2", "SHT2" }, { "",     ""     }
+};
+
 static void AddSpriteLump(lumpinfo_t *lump)
 {
     sprite_frame_t  *sprite;
@@ -281,6 +290,16 @@ static void AddSpriteLump(lumpinfo_t *lump)
 
         if (M_StringCompare(lump->name, "SHT2A0") && !BTSX)
             SHT2A0 = true;
+
+        i = 0;
+        
+        while (*weaponsprites[i].spr1)
+        {
+            if (M_StringStartsWith(lump->name, weaponsprites[i].spr1)
+                || (*weaponsprites[i].spr2 && M_StringStartsWith(lump->name, weaponsprites[i].spr2)))
+                weaponinfo[i].altered = true;
+            i++;
+        }
     }
 
     if (M_StringCompare(leafname(lump->wadfile->path), PACKAGE_WAD)

src/d_items.h
@@ -54,7 +54,7 @@ typedef struct
 
     int         motorspeed;
     int         tics;
-    dboolean    dehacked;
+    dboolean    altered;
 } weaponinfo_t;
 
 extern weaponinfo_t weaponinfo[NUMWEAPONS];

src/p_pspr.c
@@ -116,7 +116,7 @@ void P_SetPsprite(player_t *player, int position, statenum_t stnum)
         }
 
         if (state->dehacked)
-            weaponinfo[player->readyweapon].dehacked = true;
+            weaponinfo[player->readyweapon].altered = true;
 
         // Call action routine.
         // Modified handling.

src/r_things.c
@@ -942,7 +942,7 @@ void R_AddSprites(sector_t *sec, int lightlevel)
 //
 static dboolean muzzleflash;
 
-static void R_DrawPlayerSprite(pspdef_t *psp, dboolean invisibility, dboolean dehacked)
+static void R_DrawPlayerSprite(pspdef_t *psp, dboolean invisibility, dboolean altered)
 {
     fixed_t         tx;
     int             x1, x2;
@@ -956,7 +956,7 @@ static void R_DrawPlayerSprite(pspdef_t *psp, dboolean invisibility, dboolean de
     int             lump = sprframe->lump[0];
 
     // calculate edges of the shape
-    tx = psp->sx - ORIGINALWIDTH / 2 * FRACUNIT - (dehacked ? spriteoffset[lump] : newspriteoffset[lump]);
+    tx = psp->sx - ORIGINALWIDTH / 2 * FRACUNIT - (altered ? spriteoffset[lump] : newspriteoffset[lump]);
     x1 = (centerxfrac + FRACUNIT / 2 + FixedMul(tx, pspritexscale)) >> FRACBITS;
     x2 = ((centerxfrac + FRACUNIT / 2 + FixedMul(tx + spritewidth[lump], pspritexscale)) >> FRACBITS) - 1;
 
@@ -1033,7 +1033,7 @@ static void R_DrawPlayerSprite(pspdef_t *psp, dboolean invisibility, dboolean de
         {
             if (spr == SPR_SHT2)
                 vis->colfunc = ((frame & FF_FRAMEMASK & FF_FULLBRIGHT) ? tlredwhitecolfunc1 : basecolfunc);
-            else if (muzzleflash && spr <= SPR_BFGF && (!dehacked || state->translucent))
+            else if (muzzleflash && spr <= SPR_BFGF && (!altered || state->translucent))
             {
                 void (*colfuncs[])(void) =
                 {
@@ -1091,7 +1091,7 @@ static void R_DrawPlayerSprite(pspdef_t *psp, dboolean invisibility, dboolean de
 void R_DrawPlayerSprites(void)
 {
     int         invisibility = viewplayer->powers[pw_invisibility];
-    dboolean    dehacked = weaponinfo[viewplayer->readyweapon].dehacked;
+    dboolean    altered = weaponinfo[viewplayer->readyweapon].altered;
     pspdef_t    *weapon = viewplayer->psprites;
     pspdef_t    *flash = weapon + 1;
 
@@ -1101,10 +1101,10 @@ void R_DrawPlayerSprites(void)
         V_FillRect(1, viewwindowx, viewwindowy, viewwidth, viewheight, 251);
 
         if (weapon->state)
-            R_DrawPlayerSprite(weapon, true, dehacked);
+            R_DrawPlayerSprite(weapon, true, altered);
 
         if (flash->state)
-            R_DrawPlayerSprite(flash, true, dehacked);
+            R_DrawPlayerSprite(flash, true, altered);
 
         if (pausesprites)
             R_DrawPausedFuzzColumns();
@@ -1120,10 +1120,10 @@ void R_DrawPlayerSprites(void)
             muzzleflash = true;
 
         if (weapon->state)
-            R_DrawPlayerSprite(weapon, false, dehacked);
+            R_DrawPlayerSprite(weapon, false, altered);
 
         if (flash->state)
-            R_DrawPlayerSprite(flash, false, dehacked);
+            R_DrawPlayerSprite(flash, false, altered);
     }
 }
 

commit be71a89dfe0fd067fa1adf1d6b33a41a19f7fbda

Fix two issues with rocket launcher
1) A frame from DOOM's rocket launcher would appear when firing equivalent weapon in Freedoom.
2) Commit 80f48e5d15d4a9e420c8205331f23a34b8eea32f caused rocket launcher's firing frames to not be offset correctly. See issue #397. Many thanks again to @AnotherLife!

src/d_main.c
@@ -1777,7 +1777,6 @@ static void D_DoomMainSetup(void)
     if (!CheckPackageWADVersion())
         I_Error("%s is the wrong version.", packagewad);
 
-    FREEDOOM = (W_CheckNumForName("FREEDOOM") >= 0);
     FREEDM = (W_CheckNumForName("FREEDM") >= 0);
 
     DMENUPIC = (W_CheckNumForName("DMENUPIC") >= 0);

src/w_merge.c
@@ -297,13 +297,13 @@ static void AddSpriteLump(lumpinfo_t *lump)
         {
             if (M_StringStartsWith(lump->name, weaponsprites[i].spr1)
                 || (*weaponsprites[i].spr2 && M_StringStartsWith(lump->name, weaponsprites[i].spr2)))
-                weaponinfo[i].altered = true;
+                weaponinfo[i].altered = !M_StringCompare(leafname(lump->wadfile->path), PACKAGE_WAD);
+
             i++;
         }
     }
 
-    if (M_StringCompare(leafname(lump->wadfile->path), PACKAGE_WAD)
-        && (M_StringCompare(lump->name, "MISFA0") || M_StringCompare(lump->name, "MISFB0"))
+    if (M_StringCompare(leafname(lump->wadfile->path), PACKAGE_WAD) && M_StringStartsWith(lump->name, "MISF")
         && ((MISFA0 > 2 || MISFB0 > 2) || hacx || FREEDOOM))
         return;
 

src/w_wad.c
@@ -169,7 +169,8 @@ wadfile_t *W_AddFile(char *filename, dboolean automatic)
 
     M_StringCopy(wadfile->path, GetCorrectCase(filename), sizeof(wadfile->path));
 
-    wadfile->freedoom = IsFreedoom(filename);
+    if ((wadfile->freedoom = IsFreedoom(filename)))
+        FREEDOOM = true;
 
     // WAD file
     W_Read(wadfile, 0, &header, sizeof(header));