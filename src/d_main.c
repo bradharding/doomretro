370fe0a7ec80280732782a1a3248dec3643277ec
Brad Harding <brad@doomretro.com>
2017-09-10 13:18:33 +1000

    Remove extra period from devparm console message

--- src/d_main.c
+++ src/d_main.c
@@ -1657,7 +1657,7 @@ static void D_DoomMainSetup(void)
         C_Output("A <b>-fastmonsters</b> parameter was found on the command-line. Monsters will be faster.");
 
     if ((devparm = M_CheckParm("-devparm")))
-        C_Output("A <b>-devparm</b> parameter was found on the command-line. %s.", s_D_DEVSTR);
+        C_Output("A <b>-devparm</b> parameter was found on the command-line. %s", s_D_DEVSTR);
 
     // turbo option
     p = M_CheckParm("-turbo");
