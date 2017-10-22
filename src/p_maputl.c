3606ab6c4032486701aec0fcd99fb699483576d6
Brad Harding <brad@doomretro.com>
2017-10-22 17:26:45 +1100

    Simplify P_PointOnLineSide() and P_PointOnDivlineSide()

--- src/p_maputl.c
+++ src/p_maputl.c
@@ -63,9 +63,7 @@ fixed_t P_ApproxDistance(fixed_t dx, fixed_t dy)
 //
 int P_PointOnLineSide(fixed_t x, fixed_t y, line_t *line)
 {
-    return (!line->dx ? x <= line->v1->x ? line->dy > 0 : line->dy < 0 : !line->dy ?
-        y <= line->v1->y ? line->dx < 0 : line->dx > 0 : FixedMul(y - line->v1->y,
-        line->dx >> FRACBITS) >= FixedMul(line->dy >> FRACBITS, x - line->v1->x));
+    return (int)((int64_t)(y - line->v1->y) * line->dx + (int64_t)(line->v1->x - x) * line->dy >= 0);
 }
 
 //
@@ -106,9 +104,7 @@ int P_BoxOnLineSide(fixed_t *tmbox, line_t *ld)
 //
 static int P_PointOnDivlineSide(fixed_t x, fixed_t y, divline_t *line)
 {
-    return (!line->dx ? x <= line->x ? line->dy > 0 : line->dy < 0 : !line->dy ? y <= line->y ?
-        line->dx < 0 : line->dx > 0 : (line->dy ^ line->dx ^ (x -= line->x) ^ (y -= line->y)) < 0 ?
-        (line->dy ^ x) < 0 : FixedMul(y >> 8, line->dx >> 8) >= FixedMul(line->dy >> 8, x >> 8));
+    return (int)((int64_t)(y - line->y) * line->dx + (int64_t)(line->x - x) * line->dy >= 0);
 }
 
 //
