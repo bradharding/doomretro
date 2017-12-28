7298938fccd312c578e7bef7460911bdf9cc62dd
Brad Harding <brad@doomretro.com>
2017-12-29 06:35:17 +1100

    Minor tweak to drawing blood splats

--- src/r_things.c
+++ src/r_things.c
@@ -566,8 +566,7 @@ static void R_DrawBloodSplatVisSprite(const bloodsplatvissprite_t *vis)
     const rcolumn_t *columns = R_CachePatchNum(id)->columns;
 
     colfunc = vis->colfunc;
-    dc_colormap[0] = vis->colormap;
-    dc_blood = tinttab75 + (dc_colormap[0][vis->blood] << 8);
+    dc_blood = tinttab75 + (vis->colormap[vis->blood] << 8);
     spryscale = vis->scale;
     sprtopscreen = centeryfrac - FixedMul(vis->texturemid, spryscale);
     fuzzpos = 0;
