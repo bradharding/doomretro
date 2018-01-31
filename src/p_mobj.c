bradharding/doomretro

commit 56f8795c6f6a2adfe5970e24fef933536e034d1e

Don't interpolate first frame of player projectiles
See https://www.doomworld.com/forum/topic/31039-prboom-plus-ver-2514/?do=findComment&comment=1786264.

src/p_mobj.c
@@ -553,12 +553,8 @@ void P_MobjThinker(mobj_t *mobj)
     sector_t    *sector = mobj->subsector->sector;
 
     // [AM] Handle interpolation unless we're an active player.
-    if (!(player && mobj == player->mo))
+    if (!(player && mobj == player->mo) && mobj->interpolate)
     {
-        // Assume we can interpolate at the beginning
-        // of the tic.
-        mobj->interp = true;
-
         // Store starting position for mobj interpolation.
         mobj->oldx = mobj->x;
         mobj->oldy = mobj->y;
@@ -751,7 +747,9 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
     mobj->z = (z == ONFLOORZ ? mobj->floorz : (z == ONCEILINGZ ? mobj->ceilingz - height :
         BETWEEN(mobj->floorz, z, mobj->ceilingz - height)));
 
-    // [AM] Just in case interpolation is attempted...
+    if (!(mobj->flags & MF_MISSILE))
+        mobj->interpolate = true;
+
     mobj->oldx = mobj->x;
     mobj->oldy = mobj->y;
     mobj->oldz = mobj->z;

src/r_things.c
@@ -627,7 +627,7 @@ static void R_ProjectSprite(mobj_t *thing)
         return;
 
     // [AM] Interpolate between current and last position, if prudent.
-    if (thing->interp && interpolatesprites)
+    if (thing->interpolate && interpolatesprites)
     {
         fx = thing->oldx + FixedMul(thing->x - thing->oldx, fractionaltic);
         fy = thing->oldy + FixedMul(thing->y - thing->oldy, fractionaltic);
@@ -635,6 +635,7 @@ static void R_ProjectSprite(mobj_t *thing)
     }
     else
     {
+        thing->interpolate = true;
         fx = thing->x;
         fy = thing->y;
         fz = thing->z;
@@ -1165,17 +1166,17 @@ static void R_DrawBloodSplatSprite(const bloodsplatvissprite_t *splat)
 
         for (int i = r1; i <= r2; i++)
         {
-            if (bottom && clipbot[i] > ds->sprbottomclip[i])
-                clipbot[i] = ds->sprbottomclip[i];
-
             if (top && cliptop[i] < ds->sprtopclip[i])
                 cliptop[i] = ds->sprtopclip[i];
+
+            if (bottom && clipbot[i] > ds->sprbottomclip[i])
+                clipbot[i] = ds->sprbottomclip[i];
         }
     }
 
     // all clipping has been performed, so draw the bloodsplat
-    mfloorclip = clipbot;
     mceilingclip = cliptop;
+    mfloorclip = clipbot;
     R_DrawBloodSplatVisSprite(splat);
 }
 
@@ -1285,11 +1286,11 @@ static void R_DrawSprite(const vissprite_t *spr)
 
         for (int i = r1; i <= r2; i++)
         {
-            if (bottom && clipbot[i] > ds->sprbottomclip[i])
-                clipbot[i] = ds->sprbottomclip[i];
-
             if (top && cliptop[i] < ds->sprtopclip[i])
                 cliptop[i] = ds->sprtopclip[i];
+
+            if (bottom && clipbot[i] > ds->sprbottomclip[i])
+                clipbot[i] = ds->sprbottomclip[i];
         }
     }
 
@@ -1337,8 +1338,8 @@ static void R_DrawSprite(const vissprite_t *spr)
     }
 
     // all clipping has been performed, so draw the sprite
-    mfloorclip = clipbot;
     mceilingclip = cliptop;
+    mfloorclip = clipbot;
     R_DrawVisSprite(spr);
 }
 

src/p_map.c
@@ -270,7 +270,7 @@ dboolean P_TeleportMove(mobj_t *thing, fixed_t x, fixed_t y, fixed_t z, dboolean
     thing->y = y;
 
     // [AM] Don't interpolate mobjs that pass through teleporters
-    thing->interp = false;
+    thing->interpolate = false;
 
     P_SetThingPosition(thing);
 

src/p_mobj.h
@@ -323,17 +323,17 @@ typedef struct mobj_s
 
     // More list: links in sector (if needed)
     struct mobj_s       *snext;
-    struct mobj_s       **sprev;        // killough 8/10/98: change to ptr-to-ptr
+    struct mobj_s       **sprev;                // killough 8/10/98: change to ptr-to-ptr
 
     // More drawing info: to determine current sprite.
-    angle_t             angle;          // orientation
-    spritenum_t         sprite;         // used to find patch_t and flip value
-    int                 frame;          // might be ORed with FF_FULLBRIGHT
+    angle_t             angle;                  // orientation
+    spritenum_t         sprite;                 // used to find patch_t and flip value
+    int                 frame;                  // might be ORed with FF_FULLBRIGHT
 
     // Interaction info, by BLOCKMAP.
     // Links in blocks (if needed).
     struct mobj_s       *bnext;
-    struct mobj_s       **bprev;        // killough 8/11/98: change to ptr-to-ptr
+    struct mobj_s       **bprev;                // killough 8/11/98: change to ptr-to-ptr
 
     struct subsector_s  *subsector;
 
@@ -356,17 +356,17 @@ typedef struct mobj_s
     fixed_t             momz;
 
     mobjtype_t          type;
-    mobjinfo_t          *info;          // &mobjinfo[mobj->type]
+    mobjinfo_t          *info;                  // &mobjinfo[mobj->type]
 
-    int                 tics;           // state tic counter
+    int                 tics;                   // state tic counter
     state_t             *state;
     int                 flags;
     int                 flags2;
     int                 health;
 
     // Movement direction, movement generation (zig-zagging).
-    int                 movedir;        // 0-7
-    int                 movecount;      // when 0, select a new dir
+    int                 movedir;                // 0-7
+    int                 movecount;              // when 0, select a new dir
 
     // Thing being chased/attacked (or NULL),
     // also the originator for missiles.
@@ -402,14 +402,14 @@ typedef struct mobj_s
     // a linked list of sectors where this object appears
     struct msecnode_s   *touching_sectorlist;   // phares 3/14/98
 
-    short               gear;           // killough 11/98: used in torque simulation
+    short               gear;                   // killough 11/98: used in torque simulation
 
     int                 bloodsplats;
 
     int                 blood;
 
     // [AM] If true, ok to interpolate this tic.
-    dboolean            interp;
+    dboolean            interpolate;
 
     // [AM] Previous position of mobj before think.
     //      Used to interpolate between positions.

src/p_saveg.c
@@ -254,7 +254,7 @@ static void saveg_read_mobj_t(mobj_t *str)
     str->gear = saveg_read16();
     str->bloodsplats = saveg_read32();
     str->blood = saveg_read32();
-    str->interp = saveg_read32();
+    str->interpolate = saveg_read32();
     str->oldx = saveg_read32();
     str->oldy = saveg_read32();
     str->oldz = saveg_read32();
@@ -306,7 +306,7 @@ static void saveg_write_mobj_t(mobj_t *str)
     saveg_write16(str->gear);
     saveg_write32(str->bloodsplats);
     saveg_write32(str->blood);
-    saveg_write32(str->interp);
+    saveg_write32(str->interpolate);
     saveg_write32(str->oldx);
     saveg_write32(str->oldy);
     saveg_write32(str->oldz);

src/p_user.c
@@ -384,7 +384,7 @@ void P_PlayerThink(player_t *player)
         return;
 
     // [AM] Assume we can interpolate at the beginning of the tic.
-    mo->interp = true;
+    mo->interpolate = true;
 
     // [AM] Store starting position for player interpolation.
     mo->oldx = mo->x;

src/r_main.c
@@ -674,7 +674,7 @@ static void R_SetupFrame(player_t *player)
         && leveltime > 1
         // Don't interpolate if the player did something
         // that would necessitate turning it off for a tic.
-        && mo->interp
+        && mo->interpolate
         // Don't interpolate during a paused state
         && !paused && !menuactive && !consoleactive)
     {

src/r_segs.c
@@ -617,7 +617,7 @@ void R_StoreWallRange(const int start, const int stop)
             || viewz > sectors[frontsector->heightsec].interpfloorheight))
             worldbottom += animatedliquiddiff;
 
-        if (r_liquid_current && frontsector->heightsec == -1)
+        if (r_liquid_current)
         {
             frontsector->floor_xoffs = animatedliquidxoffs;
             frontsector->floor_yoffs = animatedliquidyoffs;
@@ -652,12 +652,11 @@ void R_StoreWallRange(const int start, const int stop)
         ds_p->maxscale = ds_p->scale1;
     }
 
-    // calculate texture boundaries
-    //  and decide if floor / ceiling marks are needed
+    // calculate texture boundaries and decide if floor/ceiling marks are needed
     midtexture = 0;
     toptexture = 0;
     bottomtexture = 0;
-    maskedtexture = 0;
+    maskedtexture = false;
     ds_p->maskedtexturecol = NULL;
 
     if (!backsector)

commit 4b7c2db992a8dbce1e07ba8c6756f3532a95a564

Don't interpolate teleporting monsters
This was broken back in commit 56f8795c6f6a2adfe5970e24fef933536e034d1e (Don't interpolate first frame of player projectiles).

src/p_mobj.c
@@ -540,8 +540,13 @@ void P_MobjThinker(mobj_t *mobj)
     sector_t    *sector = mobj->subsector->sector;
 
     // [AM] Handle interpolation unless we're an active player.
-    if (!(player && mobj == player->mo) && mobj->interpolate)
+    if (mobj->interpolate == -1)
+        mobj->interpolate = false;
+    else if (!(player && mobj == player->mo))
     {
+        // Assume we can interpolate at the beginning of the tic.
+        mobj->interpolate = true;
+
         // Store starting position for mobj interpolation.
         mobj->oldx = mobj->x;
         mobj->oldy = mobj->y;
@@ -1416,7 +1421,7 @@ void P_SpawnPlayerMissile(mobj_t *source, mobjtype_t type)
         th->nudge = 1;
     }
 
-    th->interpolate = false;
+    th->interpolate = -1;
 
     P_CheckMissileSpawn(th);
 

src/r_things.c
@@ -630,7 +630,6 @@ static void R_ProjectSprite(mobj_t *thing)
     }
     else
     {
-        thing->interpolate = true;
         fx = thing->x;
         fy = thing->y;
         fz = thing->z;