bradharding/doomretro

commit 0ecc24c60e1ff926a4be2a84e1a3d94e170e71bd

Fix player messages that contain a % character
Such as GOTHTHBONUS and GOTARMBONUS strings in Freedoom.

src/hu_stuff.c
@@ -1064,7 +1064,17 @@ void HU_PlayerMessage(char *message, dboolean external)
     if (message[0] == '%' && message[1] == 's')
         M_snprintf(buffer, sizeof(buffer), message, playername);
     else
-        M_StringCopy(buffer, message, sizeof(buffer));
+    {
+        size_t  len = strlen(message);
+
+        for (int i = 0, j = 0; i < len; i++)
+        {
+            if (message[i] == '%')
+                buffer[j++] = '%';
+
+            buffer[j++] = message[i];
+        }
+    }
 
     buffer[0] = toupper(buffer[0]);
     C_PlayerMessage(buffer);

commit bf87de9c9b3f5214643ae8dcbde44e25c208c65d

Fix player message containing part of prev message
Caused by commit 0ecc24c60e1ff926a4be2a84e1a3d94e170e71bd.

src/hu_stuff.c
@@ -1059,7 +1059,7 @@ void HU_SetPlayerMessage(char *message, dboolean external)
 
 void HU_PlayerMessage(char *message, dboolean external)
 {
-    static char buffer[1024];
+    char buffer[1024] = "";
 
     if (message[0] == '%' && message[1] == 's')
         M_snprintf(buffer, sizeof(buffer), message, playername);