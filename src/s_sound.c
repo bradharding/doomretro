cbb7e1ecf3a869edf78495eaefc64f45537eabcb
Brad Harding <brad@doomretro.com>
2018-08-18 08:32:31 +1000

    Allow SDL_AUDIODRIVER to be set
    Show warning if it's been changed. Thanks Fabian.

--- src/s_sound.c
+++ src/s_sound.c
@@ -176,12 +176,17 @@ void S_Init(void)
         nosfx = true;
     }
 
-#if defined(WIN32)
-    SDL_setenv("SDL_AUDIODRIVER", "DirectSound", true);
-#endif
-
     if (!nosfx)
     {
+#if defined(WIN32)
+        char    *audiobuffer = SDL_getenv("SDL_AUDIODRIVER");
+
+        if (audiobuffer)
+            C_Warning("The <b>SDL_AUDIODRIVER</b> environment variable has been set to <b>\"%s\"</b>.", audiobuffer);
+
+        SDL_setenv("SDL_AUDIODRIVER", "DirectSound", false);
+#endif
+
         InitSfxModule();
         S_SetSfxVolume(sfxVolume * MAX_SFX_VOLUME / 31);
 
