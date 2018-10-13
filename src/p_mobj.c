d1d98d0a58ac6e87e57da9d13d3e02667c23e585
Brad Harding <brad@doomretro.com>
2018-10-14 09:38:18 +1100

    Fix interpolation of some items spawned on moving sectors

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -747,6 +747,7 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
     mobj->oldy = mobj->y;
     mobj->oldz = mobj->z;
     mobj->oldangle = mobj->angle;
+    mobj->interpolate = true;
 
     mobj->thinker.function = (type == MT_MUSICSOURCE ? MusInfoThinker : P_MobjThinker);
     P_AddThinker(&mobj->thinker);
