352033fc94745e0cc754e7014b265ab54995bbea
Brad Harding <brad@doomretro.com>
2018-11-06 19:44:12 +1100

    Fix Clang/GCC compilation

--- src/i_gamepad.h
+++ src/i_gamepad.h
@@ -41,31 +41,33 @@
 
 #include "doomtype.h"
 
-#define GAMEPAD_DPAD_UP         0x0001
-#define GAMEPAD_DPAD_DOWN       0x0002
-#define GAMEPAD_DPAD_LEFT       0x0004
-#define GAMEPAD_DPAD_RIGHT      0x0008
-#define GAMEPAD_START           0x0010
-#define GAMEPAD_BACK            0x0020
-#define GAMEPAD_LEFT_THUMB      0x0040
-#define GAMEPAD_RIGHT_THUMB     0x0080
-#define GAMEPAD_LEFT_SHOULDER   0x0100
-#define GAMEPAD_RIGHT_SHOULDER  0x0200
-#define GAMEPAD_LEFT_TRIGGER    0x0400
-#define GAMEPAD_RIGHT_TRIGGER   0x0800
-#define GAMEPAD_A               0x1000
-#define GAMEPAD_B               0x2000
-#define GAMEPAD_X               0x4000
-#define GAMEPAD_Y               0x8000
+#define GAMEPAD_DPAD_UP             0x0001
+#define GAMEPAD_DPAD_DOWN           0x0002
+#define GAMEPAD_DPAD_LEFT           0x0004
+#define GAMEPAD_DPAD_RIGHT          0x0008
+#define GAMEPAD_START               0x0010
+#define GAMEPAD_BACK                0x0020
+#define GAMEPAD_LEFT_THUMB          0x0040
+#define GAMEPAD_RIGHT_THUMB         0x0080
+#define GAMEPAD_LEFT_SHOULDER       0x0100
+#define GAMEPAD_RIGHT_SHOULDER      0x0200
+#define GAMEPAD_LEFT_TRIGGER        0x0400
+#define GAMEPAD_RIGHT_TRIGGER       0x0800
+#define GAMEPAD_A                   0x1000
+#define GAMEPAD_B                   0x2000
+#define GAMEPAD_X                   0x4000
+#define GAMEPAD_Y                   0x8000
 
-#define gamepadthumbLXleft      (float)(-gamepadthumbLX - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
-#define gamepadthumbLXright     (float)(gamepadthumbLX - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
-#define gamepadthumbLYup        (float)(-gamepadthumbLY - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
-#define gamepadthumbLYdown      (float)(gamepadthumbLY - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
-#define gamepadthumbRXleft      pow((-gamepadthumbRX - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone), 3.0f)
-#define gamepadthumbRXright     pow((gamepadthumbRX - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone), 3.0f)
-#define gamepadthumbRYup        (-(float)(-gamepadthumbRY - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone))
-#define gamepadthumbRYdown      (float)(gamepadthumbRY - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone)
+#define GAMEPAD_TRIGGER_THRESHOLD   30
+
+#define gamepadthumbLXleft          (float)(-gamepadthumbLX - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
+#define gamepadthumbLXright         (float)(gamepadthumbLX - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
+#define gamepadthumbLYup            (float)(-gamepadthumbLY - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
+#define gamepadthumbLYdown          (float)(gamepadthumbLY - gamepadleftdeadzone) / ((float)SHRT_MAX - gamepadleftdeadzone)
+#define gamepadthumbRXleft          pow((-gamepadthumbRX - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone), 3.0f)
+#define gamepadthumbRXright         pow((gamepadthumbRX - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone), 3.0f)
+#define gamepadthumbRYup            (-(float)(-gamepadthumbRY - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone))
+#define gamepadthumbRYdown          (float)(gamepadthumbRY - gamepadrightdeadzone) / ((float)SHRT_MAX - gamepadrightdeadzone)
 
 extern int      barrelvibrationtics;
 extern int      damagevibrationtics;
