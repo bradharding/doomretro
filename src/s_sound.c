99dbf527034c8d36f88b729ec44553e10a8478e5
Brad Harding <brad@doomretro.com>
2018-11-16 07:10:22 +1100

    Remove audio device name from console at startup

--- src/s_sound.c
+++ src/s_sound.c
@@ -130,7 +130,6 @@ static void InitSfxModule(void)
 {
     if (I_InitSound())
     {
-        C_Output("Using the audio device called \"%s\".", SDL_GetAudioDeviceName(0, 0));
         C_Output("Sound effects will play at a sample rate of %.1fkHz over %i channels%s.", SAMPLERATE / 1000.0f, s_channels,
             (M_StringCompare(SDL_GetCurrentAudioDriver(), "directsound") ? " using the <i><b>DirectSound</b></i> API" : ""));
         return;
