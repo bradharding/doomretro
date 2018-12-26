6322a2a6f895da5cbaa8d66f2649044ed3f972c3
mfrancis95 <mikefrancis95@gmail.com>
2018-12-26 12:06:09 -0500

    Fix two memory leaks in D_SetSaveGameFolder

--- src/d_iwad.c
+++ src/d_iwad.c
@@ -542,7 +542,10 @@ void D_SetSaveGameFolder(dboolean output)
         M_MakeDirectory(appdatafolder);
         savegamefolder = M_StringJoin(appdatafolder, DIR_SEPARATOR_S, "savegames", DIR_SEPARATOR_S, NULL);
         M_MakeDirectory(savegamefolder);
+        char *savegamefolder_free = savegamefolder;
         savegamefolder = M_StringJoin(savegamefolder, (*pwadfile ? pwadfile : iwad_name), DIR_SEPARATOR_S, NULL);
+        free(appdatafolder);
+        free(savegamefolder_free);
     }
 
     M_MakeDirectory(savegamefolder);
