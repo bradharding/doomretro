7fb484e415bcde332d438d40fd95f9a59d75b6b0
Brad Harding <brad@doomretro.com>
2018-12-28 16:07:45 +1100

    Add legal trademarks to Windows executable

--- src/version.h
+++ src/version.h
@@ -53,6 +53,10 @@
 #define PACKAGE_MUTEX                   "DOOMRETRO-CC4F1071-8B24-4E91-A207-D792F39636CD"
 #define PACKAGE_NAME                    "DOOM Retro"
 #define PACKAGE_SAVE                    "doomretro%i.save"
+#define PACKAGE_TRADEMARKS              "DOOM is a registered trademark of id Software LLC, a ZeniMax Media company, in the US " \
+                                        "and/or other countries, and is used without permission.All other trademarks are the " \
+                                        "property of their respective holders. DOOM Retro is in no way affiliated with nor endorsed " \
+                                        "by id Software."
 #define PACKAGE_WAD                     "doomretro.wad"
 #define PACKAGE_WIKI_HELP_URL           "https://github.com/bradharding/doomretro/wiki/THE-CONSOLE"
 
