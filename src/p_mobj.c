bradharding/doomretro

commit bafcb36bb48505a9f3a081fa77b0e7342b4f8db8

Minor tweaks

src/p_mobj.c
@@ -544,15 +544,14 @@ void P_MobjThinker(mobj_t *mobj)
     int         flags = mobj->flags;
     int         flags2 = mobj->flags2;
     player_t    *player = mobj->player;
-    sector_t    *sector = mobj->subsector->sector;
 
     // [AM] Handle interpolation unless we're an active player.
     if (mobj->interpolate == -1)
-        mobj->interpolate = false;
+        mobj->interpolate = 0;
     else if (!(player && mobj == player->mo))
     {
         // Assume we can interpolate at the beginning of the tic.
-        mobj->interpolate = true;
+        mobj->interpolate = 1;
 
         // Store starting position for mobj interpolation.
         mobj->oldx = mobj->x;
@@ -574,9 +573,13 @@ void P_MobjThinker(mobj_t *mobj)
     }
 
     // [BH] bob objects in liquid
-    if ((flags2 & MF2_FEETARECLIPPED) && !(flags2 & MF2_NOLIQUIDBOB) && mobj->z <= sector->floorheight && !mobj->momz
-        && !sector->heightsec && r_liquid_bob)
-        mobj->z += animatedliquiddiffs[(mobj->floatbob + leveltime) & 63];
+    if ((flags2 & MF2_FEETARECLIPPED) && !(flags2 & MF2_NOLIQUIDBOB) && r_liquid_bob)
+    {
+        sector_t    *sector = mobj->subsector->sector;
+
+        if (mobj->z <= sector->floorheight && !mobj->momz && !sector->heightsec)
+            mobj->z += animatedliquiddiffs[(mobj->floatbob + leveltime) & 63];
+    }
 
     // [BH] otherwise bob certain power-ups
     else if ((flags2 & MF2_FLOATBOB) && r_floatbob)
@@ -919,7 +922,7 @@ static void P_SpawnPlayer(const mapthing_t *mthing)
     viewplayer->fixedcolormap = 0;
     viewplayer->viewheight = VIEWHEIGHT;
 
-    viewplayer->viewz = viewplayer->mo->z + viewplayer->viewheight;
+    viewplayer->viewz = viewplayer->oldviewz = viewplayer->mo->z + viewplayer->viewheight;
     viewplayer->psprites[ps_weapon].sx = 0;
     viewplayer->mo->momx = 0;
     viewplayer->mo->momy = 0;

src/c_cmds.c
@@ -3108,7 +3108,7 @@ static void mapstats_cmd_func2(char *cmd, char *parms)
 
     C_Header(tabs, MAPSTATSTITLE);
 
-    C_TabbedOutput(tabs, "Title\t<b><i>%s</i></b>", maptitle);
+    C_TabbedOutput(tabs, "Title\t<b><i>%s</i></b>", titlecase(maptitle));
 
     {
         const char *authors[][6] =

src/p_map.c
@@ -270,7 +270,7 @@ dboolean P_TeleportMove(mobj_t *thing, fixed_t x, fixed_t y, fixed_t z, dboolean
     thing->y = y;
 
     // [AM] Don't interpolate mobjs that pass through teleporters
-    thing->interpolate = false;
+    thing->interpolate = 0;
 
     P_SetThingPosition(thing);
 

src/p_user.c
@@ -524,7 +524,7 @@ void P_PlayerThink(void)
         return;
 
     // [AM] Assume we can interpolate at the beginning of the tic.
-    mo->interpolate = true;
+    mo->interpolate = 1;
 
     // [AM] Store starting position for player interpolation.
     mo->oldx = mo->x;

src/r_main.c
@@ -746,9 +746,6 @@ static void R_SetupFrame(void)
 
     // [AM] Interpolate the player camera if the feature is enabled.
     if (vid_capfps != TICRATE
-        // Don't interpolate on the first tic of a level, otherwise
-        // oldviewz might be garbage.
-        && leveltime > 1
         // Don't interpolate if the player did something
         // that would necessitate turning it off for a tic.
         && mo->interpolate

commit f61a7f297a276963e42447a6c43d4032621463cf

Fix issue clipping corpses in liquid
Caused by change made in bafcb36bb48505a9f3a081fa77b0e7342b4f8db8.

src/p_mobj.c
@@ -544,6 +544,7 @@ void P_MobjThinker(mobj_t *mobj)
     int         flags = mobj->flags;
     int         flags2 = mobj->flags2;
     player_t    *player = mobj->player;
+    sector_t    *sector = mobj->subsector->sector;
 
     // [AM] Handle interpolation unless we're an active player.
     if (mobj->interpolate == -1)
@@ -573,13 +574,9 @@ void P_MobjThinker(mobj_t *mobj)
     }
 
     // [BH] bob objects in liquid
-    if ((flags2 & MF2_FEETARECLIPPED) && !(flags2 & MF2_NOLIQUIDBOB) && r_liquid_bob)
-    {
-        sector_t    *sector = mobj->subsector->sector;
-
-        if (mobj->z <= sector->floorheight && !mobj->momz && !sector->heightsec)
-            mobj->z += animatedliquiddiffs[(mobj->floatbob + leveltime) & 63];
-    }
+    if ((flags2 & MF2_FEETARECLIPPED) && !(flags2 & MF2_NOLIQUIDBOB) && mobj->z <= sector->floorheight && !mobj->momz
+        && !sector->heightsec && r_liquid_bob)
+        mobj->z += animatedliquiddiffs[(mobj->floatbob + leveltime) & 63];
 
     // [BH] otherwise bob certain power-ups
     else if ((flags2 & MF2_FLOATBOB) && r_floatbob)