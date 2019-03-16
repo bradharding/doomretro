51004af0154da0a373f692c2465c21c750e88600
Brad Harding <brad@doomretro.com>
2019-03-16 15:52:33 +1100

    Use _str[n]icmp instead of deprecated str[n]icmp

--- src/doomdef.h
+++ src/doomdef.h
@@ -44,8 +44,8 @@
 #include "m_controls.h"
 
 #if defined(_WIN32)
-#define strcasecmp  stricmp
-#define strncasecmp strnicmp
+#define strcasecmp  _stricmp
+#define strncasecmp _strnicmp
 #else
 #include <strings.h>
 #endif
