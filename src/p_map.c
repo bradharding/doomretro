bradharding/doomretro

commit dfb39bd4461a1e4e0f92554e123641ccbe14abd2

Fix flying monsters over moving, liquid sectors

src/p_map.c
@@ -915,6 +915,9 @@ void P_FakeZMovement(mobj_t *mo)
 
 dboolean P_IsInLiquid(mobj_t *thing)
 {
+    if (thing->z > thing->floorz)
+        return false;
+
     for (const struct msecnode_s *seclist = thing->touching_sectorlist; seclist; seclist = seclist->m_tnext)
         if (seclist->m_sector->terraintype == SOLID)
             return false;

src/r_data.c
@@ -268,7 +268,6 @@ static void R_InitTextures(void)
     pnameslump_t        *pnameslumps = malloc(sizeof(*pnameslumps));
     int                 maxpnameslumps = 1;
     int                 numpnameslumps = 0;
-
     const maptexture_t  *mtexture;
     texture_t           *texture;
     int                 maptex_lump[2] = { -1, -1 };

commit 8e42257e5b5470101d45a3651b361e5074234524

Flying monsters can't be clipped in liquid
Fixes problems caused by commit dfb39bd4461a1e4e0f92554e123641ccbe14abd2.

src/p_map.c
@@ -916,7 +916,7 @@ void P_FakeZMovement(mobj_t *mo)
 
 dboolean P_IsInLiquid(mobj_t *thing)
 {
-    if (thing->z > thing->floorz)
+    if (thing->flags & MF_NOGRAVITY)
         return false;
 
     for (const struct msecnode_s *seclist = thing->touching_sectorlist; seclist; seclist = seclist->m_tnext)