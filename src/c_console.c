c7ab35fdf4d09415562ed26e1aef8f14486f8c02
Brad Harding <brad@doomretro.com>
2019-11-24 17:15:06 +1100

    Don't italicize '+' character in console

--- src/c_console.c
+++ src/c_console.c
@@ -955,7 +955,7 @@ static int C_DrawConsoleText(int x, int y, char *text, const int color1, const i
 
                 V_DrawConsoleTextPatch(x, y, patch, patchwidth, (lastcolor1 = (bold == 1 ? boldcolor : (bold == 2 ? color1 : (italics ?
                     (color1 == consolewarningcolor ? color1 : consoleitalicscolor) : color1)))), color2,
-                    (italics && letter != '_' && letter != '-' && letter != ',' && letter != '/'), translucency);
+                    (italics && letter != '_' && letter != '-' && letter != '+' && letter != ',' && letter != '/'), translucency);
                 x += patchwidth;
             }
 
