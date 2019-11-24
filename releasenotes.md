a4501ca58792666b01f9004ef52d6bdebc87391b
Brad Harding <brad@doomretro.com>
2019-11-25 09:03:51 +1100

    Bump version for next release

--- releasenotes.md
+++ releasenotes.md
@@ -1,3 +1,9 @@
+### DOOM Retro v3.2.1
+
+---
+
+###### Monday, November 25, 2019
+
 ### DOOM Retro v3.2
 
 * Optimizations have been made to further improve the overall performance and stability of *DOOM Retro*.
--- src/version.h
+++ src/version.h
@@ -39,9 +39,9 @@
 #if !defined(__VERSION_H__)
 #define __VERSION_H__
 
-#define PACKAGE_VERSION                 3,2,0,0
-#define PACKAGE_VERSIONSTRING           "3.2"
-#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v3.2"
+#define PACKAGE_VERSION                 3,2,1,0
+#define PACKAGE_VERSIONSTRING           "3.2.1"
+#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v3.2.1"
 #define PACKAGE_SAVEGAMEVERSIONSTRING   "DOOM Retro v3.0"
 
 #define PACKAGE                         "doomretro"
