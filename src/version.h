e422530bda5363d3723f7ec16b0d5855526d3e19
Brad Harding <brad@doomretro.com>
2019-12-01 11:05:09 +1100

    Update copyright year
    The next version of DOOM Retro won't be until early next year.

--- src/version.h
+++ src/version.h
@@ -7,7 +7,7 @@
 ========================================================================
 
   Copyright © 1993-2012 by id Software LLC, a ZeniMax Media company.
-  Copyright © 2013-2019 by Brad Harding.
+  Copyright © 2013-2020 by Brad Harding.
 
   DOOM Retro is a fork of Chocolate DOOM. For a list of credits, see
   <https://github.com/bradharding/doomretro/wiki/CREDITS>.
@@ -47,7 +47,7 @@
 #define PACKAGE                         "doomretro"
 #define PACKAGE_AUTHOR                  "Brad Harding <brad@doomretro.com>"
 #define PACKAGE_CONFIG                  "doomretro.cfg"
-#define PACKAGE_COPYRIGHT               "\xA9 2013\x962019 by Brad Harding. All rights reserved."
+#define PACKAGE_COPYRIGHT               "\xA9 2013\x962020 by Brad Harding. All rights reserved."
 #define PACKAGE_FILENAME                "doomretro.exe"
 #define PACKAGE_ICON_PATH               "..\\res\\doomretro.ico"
 #define PACKAGE_MUTEX                   "DOOMRETRO-CC4F1071-8B24-4E91-A207-D792F39636CD"
