426737c0237c80aa0736a72e50b6ed3aab1af432
Brad Harding <brad@doomretro.com>
2019-12-28 19:36:25 +1100

    Bump version for next release

--- releasenotes.md
+++ releasenotes.md
@@ -1,4 +1,4 @@
-### DOOM Retro v3.3.1
+### DOOM Retro v3.4
 
 * Optimizations have been made to further improve the overall performance and stability of *DOOM Retro*.
 * Minor changes have been made to text that is output to the console.
--- src/version.h
+++ src/version.h
@@ -39,9 +39,9 @@
 #if !defined(__VERSION_H__)
 #define __VERSION_H__
 
-#define PACKAGE_VERSION                 3,3,1,0
-#define PACKAGE_VERSIONSTRING           "3.3.1"
-#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v3.3.1"
+#define PACKAGE_VERSION                 3,4,0,0
+#define PACKAGE_VERSIONSTRING           "3.4"
+#define PACKAGE_NAMEANDVERSIONSTRING    "DOOM Retro v3.4"
 #define PACKAGE_SAVEGAMEVERSIONSTRING   "DOOM Retro v3.0"
 
 #define PACKAGE                         "doomretro"
