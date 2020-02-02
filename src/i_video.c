a1a60ad060835eac9066d42b96339534dd847527
Brad Harding <brad@doomretro.com>
2020-02-02 16:13:50 +1100

    Play DSSTNMOV when toggling fullscreen

--- src/i_video.c
+++ src/i_video.c
@@ -1851,6 +1851,8 @@ void I_ToggleFullscreen(void)
     if (nearestlinear)
         I_UpdateBlitFunc(viewplayer && viewplayer->damagecount);
 
+    S_StartSound(NULL, sfx_stnmov);
+
     if (vid_fullscreen)
         C_StrCVAROutput(stringize(vid_fullscreen), "on");
     else
