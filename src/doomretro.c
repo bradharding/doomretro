db057fca64202c38363fb1546c650195afb8f26f
Brad Harding <brad@doomretro.com>
2020-03-08 15:37:35 +1100

    Remove broken exception handler

--- src/doomretro.c
+++ src/doomretro.c
@@ -179,13 +179,6 @@ static void I_AccessibilityShortcutKeys(dboolean bAllowKeys)
     }
 }
 
-LONG WINAPI ExceptionHandler(LPEXCEPTION_POINTERS info)
-{
-    I_MidiRPCClientShutDown();
-
-    return EXCEPTION_EXECUTE_HANDLER;
-}
-
 void I_InitWindows32(void)
 {
     HINSTANCE       handle = GetModuleHandle(NULL);
@@ -206,8 +199,6 @@ void I_InitWindows32(void)
 
     windowborderwidth = (GetSystemMetrics(SM_CXFRAME) + GetSystemMetrics(SM_CXPADDEDBORDER)) * 2;
     windowborderheight = (GetSystemMetrics(SM_CYFRAME) + GetSystemMetrics(SM_CXPADDEDBORDER)) * 2 + GetSystemMetrics(SM_CYCAPTION);
-
-    SetUnhandledExceptionFilter(ExceptionHandler);
 }
 
 void I_ShutdownWindows32(void)
