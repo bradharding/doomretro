063e0fcd0c638bd7c9e131508e3708d09d20a20e
Brad Harding <brad@doomretro.com>
2020-05-09 18:38:30 +1000

    Fix potential crash

--- src/c_console.c
+++ src/c_console.c
@@ -880,13 +880,13 @@ static void C_DrawBackground(void)
         for (int i = 0; i < height - 3 * CONSOLEWIDTH; i += CONSOLEWIDTH)
             screens[0][i] = consoleautomapbevel[screens[0][i + 1]];
 
-        for (int i = height - 3 * CONSOLEWIDTH; i < height; i += CONSOLEWIDTH)
+        for (int i = MAX(0, height - 3 * CONSOLEWIDTH); i < height; i += CONSOLEWIDTH)
             screens[0][i] = consolebevel[screens[0][i + 1]];
 
         for (int i = 0; i < height - (brandheight + 3) * CONSOLEWIDTH; i += CONSOLEWIDTH)
             screens[0][i + CONSOLEWIDTH - 1] = consoleautomapbevel[screens[0][i + CONSOLEWIDTH - 2]];
 
-        for (int i = height - (brandheight + 3) * CONSOLEWIDTH; i < height; i += CONSOLEWIDTH)
+        for (int i = MAX(0, height - (brandheight + 3) * CONSOLEWIDTH); i < height; i += CONSOLEWIDTH)
             screens[0][i + CONSOLEWIDTH - 1] = consolebevel[screens[0][i + CONSOLEWIDTH - 2]];
     }
     else
