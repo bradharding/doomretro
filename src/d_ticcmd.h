64fb2df1e426e36b72cb001e0a46cd96522a23a9
Brad Harding <brad@doomretro.com>
2020-05-23 06:25:51 +1000

    Fix player movement issues on ARM
    Thank you to @vanfanel! See issue #590 for the journey.

--- src/d_ticcmd.h
+++ src/d_ticcmd.h
@@ -41,13 +41,14 @@
 
 // The data sampled per tic..
 // Mainly movements/button commands per game tic.
+// [BH] forwardmove, sidemove and angleturn need to be explicitly signed
 typedef struct
 {
-    char    forwardmove;    // * 2048 for move
-    char    sidemove;       // * 2048 for move
-    short   angleturn;      // << 16 for angle delta
-    byte    buttons;
-    int     lookdir;
+    signed char     forwardmove;    // * 2048 for move
+    signed char     sidemove;       // * 2048 for move
+    signed short    angleturn;      // << 16 for angle delta
+    byte            buttons;
+    int             lookdir;
 } ticcmd_t;
 
 #endif
