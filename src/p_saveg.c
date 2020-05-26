c59f3237aecd046ea2198f9c6892195c1b614c81
Brad Harding <brad@doomretro.com>
2020-05-26 15:06:38 +1000

    Fix thinker not being restored for plats on loading savegame

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -1522,6 +1522,7 @@ void P_UnArchiveSpecials(void)
 
                 saveg_read_plat_t(plat);
                 plat->sector->floordata = plat;
+                plat->thinker.function = T_PlatRaise;
                 P_AddThinker(&plat->thinker);
                 P_AddActivePlat(plat);
                 break;
