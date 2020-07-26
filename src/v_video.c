8769c196306425fdf8e10a07f69a0ed0d2e448c9
Brad Harding <brad@doomretro.com>
2020-07-26 19:48:00 +1000

    More minor tweaks

--- src/v_video.c
+++ src/v_video.c
@@ -466,8 +466,7 @@ void V_DrawBigPatch(int x, int y, patch_t *patch)
 void V_DrawConsoleInputTextPatch(int x, int y, patch_t *patch, int width, int color,
     int backgroundcolor, dboolean italics, byte *translucency)
 {
-    byte        *desttop = &screens[0][y * SCREENWIDTH + x];
-    const int   italicize[] = { 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, -1, -1, -1 };
+    byte    *desttop = &screens[0][y * SCREENWIDTH + x];
 
     for (int col = 0; col < width; col++, desttop++)
     {
@@ -502,19 +501,19 @@ void V_DrawConsoleInputTextPatch(int x, int y, patch_t *patch, int width, int co
 void V_DrawConsoleOutputTextPatch(int x, int y, patch_t *patch, int width, int color,
     int backgroundcolor, dboolean italics, byte *translucency)
 {
-    byte *desttop = &screens[0][y * SCREENWIDTH + x];
+    byte        *desttop = &screens[0][y * SCREENWIDTH + x];
     const int   italicize[] = { 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, -1, -1, -1 };
 
     for (int col = 0; col < width; col++, desttop++)
     {
-        column_t *column = (column_t *)((byte *)patch + LONG(patch->columnofs[col]));
+        column_t    *column = (column_t *)((byte *)patch + LONG(patch->columnofs[col]));
         byte        topdelta;
 
         // step through the posts in a column
         while ((topdelta = column->topdelta) != 0xFF)
         {
-            byte *source = (byte *)column + 3;
-            byte *dest = &desttop[topdelta * SCREENWIDTH];
+            byte    *source = (byte *)column + 3;
+            byte    *dest = &desttop[topdelta * SCREENWIDTH];
 
             for (int i = 0; i < CONSOLELINEHEIGHT; i++)
             {
