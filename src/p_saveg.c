0de8c7b49f21d4ad741e467ff8ce4046eaef5145
Brad Harding <brad@doomretro.com>
2020-08-16 11:28:54 +1000

    Remove unncessary call to P_RemoveThinkerDelayed()

--- src/p_saveg.c
+++ src/p_saveg.c
@@ -1325,10 +1325,7 @@ void P_UnArchiveThinkers(void)
         thinker_t   *next = currentthinker->next;
 
         if (currentthinker->function == &P_MobjThinker || currentthinker->function == &MusInfoThinker)
-        {
             P_RemoveMobj((mobj_t *)currentthinker);
-            P_RemoveThinkerDelayed(currentthinker);
-        }
         else
             Z_Free(currentthinker);
 
