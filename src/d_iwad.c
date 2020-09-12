29ac0a1a0f0f1bc3fa79de0db8900631bb96b73d
Brad Harding <brad@doomretro.com>
2020-09-12 17:09:43 +1000

    Find DOOM installations better for 64-bit builds

--- src/d_iwad.c
+++ src/d_iwad.c
@@ -87,33 +87,42 @@ typedef struct
 // C:\Program Files\Path\uninstl.exe /S C:\Program Files\Path
 //
 // With some munging we can find where DOOM was installed.
+
+// [AlexMax] From the perspective of a 64-bit executable, 32-bit registry
+// keys are located in a different spot.
+#if defined(_WIN64)
+#define SOFTWARE_KEY    "Software\\Wow6432Node"
+#else
+#define SOFTWARE_KEY    "Software"
+#endif
+
 static registryvalue_t uninstall_values[] =
 {
     // Ultimate DOOM, CD version (Depths of DOOM trilogy)
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Ultimate Doom for Windows 95",
+        SOFTWARE_KEY "\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Ultimate Doom for Windows 95",
         "UninstallString"
     },
 
     // DOOM II, CD version (Depths of DOOM trilogy)
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Doom II for Windows 95",
+        SOFTWARE_KEY "\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Doom II for Windows 95",
         "UninstallString"
     },
 
     // Final DOOM
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Final Doom for Windows 95",
+        SOFTWARE_KEY "\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Final Doom for Windows 95",
         "UninstallString"
     },
 
     // Shareware version
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Doom Shareware for Windows 95",
+        SOFTWARE_KEY "\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Doom Shareware for Windows 95",
         "UninstallString"
     },
 };
@@ -124,35 +133,35 @@ static registryvalue_t root_path_keys[] =
     // DOOM Collector's Edition
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\Activision\\DOOM Collector's Edition\\v1.0",
+        SOFTWARE_KEY "\\Activision\\DOOM Collector's Edition\\v1.0",
         "INSTALLPATH"
     },
 
     // DOOM II
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\GOG.com\\Games\\1435848814",
+        SOFTWARE_KEY "\\GOG.com\\Games\\1435848814",
         "PATH"
     },
 
     // DOOM 3: BFG Edition
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\GOG.com\\Games\\1135892318",
+        SOFTWARE_KEY "\\GOG.com\\Games\\1135892318",
         "PATH"
     },
 
     // Final DOOM
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\GOG.com\\Games\\1435848742",
+        SOFTWARE_KEY "\\GOG.com\\Games\\1435848742",
         "PATH"
     },
 
     // Ultimate DOOM
     {
         HKEY_LOCAL_MACHINE,
-        "Software\\GOG.com\\Games\\1435827232",
+        SOFTWARE_KEY "\\GOG.com\\Games\\1435827232",
         "PATH"
     }
 };
@@ -173,8 +182,8 @@ static const char *root_path_subdirs[] =
 static registryvalue_t steam_install_location =
 {
     HKEY_CURRENT_USER,
-    "Software\\Valve\\Steam",
-    "SteamPath"
+    SOFTWARE_KEY "\\Valve\\Steam",
+    "InstallPath"
 };
 
 // Subdirs of the steam install directory where IWADs are found
