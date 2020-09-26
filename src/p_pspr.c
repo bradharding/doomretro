80d4f1f285afd54c14e99ccd88f3b82066c05464
Brad Harding <brad@doomretro.com>
2020-09-26 18:37:39 +1000

    Remove recent change to A_FireOldBFG()

--- src/p_pspr.c
+++ src/p_pspr.c
@@ -509,9 +509,6 @@ void A_FireBFG(mobj_t *actor, player_t *player, pspdef_t *psp)
 //
 void A_FireOldBFG(mobj_t *actor, player_t *player, pspdef_t *psp)
 {
-    if (!player)
-        return;
-
     P_SubtractAmmo(1);
 
     player->extralight = 2;
