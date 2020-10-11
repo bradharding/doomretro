246f2a6c82eeee24f1350e7eea4c04d506d156a7
Brad Harding <brad@doomretro.com>
2020-10-11 19:21:44 +1100

    Fix previous commit

--- src/m_menu.c
+++ src/m_menu.c
@@ -2864,7 +2864,7 @@ dboolean M_Responder(event_t *ev)
 
             if (!viewactive)
             {
-                if (r_screensize == 8)
+                if (r_screensize >= 7 && vid_widescreen)
                     r_screensize = 7;
                 else
                     return false;
