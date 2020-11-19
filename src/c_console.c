6c6eec2142443989d0c5b9f52e7d09617cda6c9c
Brad Harding <brad@doomretro.com>
2020-11-19 16:19:02 +1100

    Fix bold color in console warnings

--- src/c_console.c
+++ src/c_console.c
@@ -1171,7 +1171,7 @@ static int C_DrawConsoleText(int x, int y, char *text, const int color1, const i
                 int patchwidth = SHORT(patch->width);
 
                 consoletextfunc(x, y, patch, patchwidth, (lastcolor1 = (bold && italics ? (color1 == consolewarningcolor ? color1 :
-                    consolebolditalicscolor) : (bold ? consoleboldcolor : color1))), color2, (italics && letter != '_' && letter != '-'
+                    consolebolditalicscolor) : (bold ? boldcolor : color1))), color2, (italics && letter != '_' && letter != '-'
                     && letter != '+' && letter != ',' && letter != '/'), translucency);
                 x += patchwidth;
             }
