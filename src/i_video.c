72ae567f4d7db28ec25e864d304d59dbe3587167
Brad Harding <brad@doomretro.com>
2021-03-13 17:57:31 +1100

    Fix miscalculation of scaled screen width (take 2)

--- src/i_video.c
+++ src/i_video.c
@@ -1565,7 +1565,7 @@ static void SetVideoMode(dboolean createwindow, dboolean output)
         }
     }
 
-    if (SDL_RenderSetLogicalSize(renderer, 0, ACTUALHEIGHT) < 0)
+    if (SDL_RenderSetLogicalSize(renderer, SCREENWIDTH * !vid_widescreen, ACTUALHEIGHT) < 0)
         I_SDLError(SDL_RenderSetLogicalSize);
 
     if (output)
