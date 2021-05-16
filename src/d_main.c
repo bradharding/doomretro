9d2a0c3e80df94f6d84e94bfb81a5d9be9615bb9
Brad Harding <brad@doomretro.com>
2021-05-16 18:35:11 +1000

    Minor tweaks to drawing splash screen

--- src/d_main.c
+++ src/d_main.c
@@ -535,17 +535,11 @@ void D_PageDrawer(void)
 {
     if (splashscreen)
     {
-        static int  prevtic;
+        if (logotic >= 77 && logotic < 94)
+            V_DrawBigPatch((SCREENWIDTH - NONWIDEWIDTH) / 2 + 143, 167, logolump[94 - logotic]);
 
-        if (prevtic != pagetic)
-        {
-            if (logotic >= 77 && logotic < 94)
-                V_DrawBigPatch((SCREENWIDTH - NONWIDEWIDTH) / 2 + 143, 167, logolump[94 - logotic]);
-
-            V_DrawBigPatch((SCREENWIDTH - NONWIDEWIDTH) / 2 + 12, 366, fineprintlump);
-            I_SetSimplePalette(&splashpal[(pagetic < 9 ? 9 - pagetic : (pagetic > 94 ? pagetic - 94 : 0)) * 768]);
-            prevtic = pagetic;
-        }
+        V_DrawBigPatch((SCREENWIDTH - NONWIDEWIDTH) / 2 + 12, 366, fineprintlump);
+        I_SetSimplePalette(&splashpal[(pagetic < 9 ? 9 - pagetic : (pagetic > 94 ? pagetic - 94 : 0)) * 768]);
     }
     else
     {
