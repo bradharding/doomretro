94f499bc3c4942dae5552c8efec2c64d8b22a42d
Brad Harding <brad@doomretro.com>
2021-08-05 17:06:14 +1000

    Fix typo in previous commit

--- src/c_cmds.c
+++ src/c_cmds.c
@@ -8847,7 +8847,7 @@ static void vid_capfps_cvar_func2(char *cmd, char *parms)
         {
             vid_capfps = value;
             M_SaveCVARs();
-            I_CapFPS(consoleactive TICRATE : vid_capfps);
+            I_CapFPS(consoleactive ? TICRATE : vid_capfps);
         }
     }
     else
