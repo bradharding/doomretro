5e36f4faa4402aa0be0b85aaa2a1aac420b48aef
Brad Harding <brad@doomretro.com>
2021-08-11 15:13:21 +1000

    Update to final release of SDL 2.0.16

--- SDL2-2.0.16/include/SDL_revision.h
+++ SDL2-2.0.16/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@a91ab883e970611cef0924df8aec7d264f93c8d1"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@25f9ed87ff6947d9576fc9d79dee0784e638ac58"
 #define SDL_REVISION_NUMBER 0
