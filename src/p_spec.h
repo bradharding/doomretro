3f0426c546641c69adacb93ee4fe9203ef162421
Brad Harding <brad@doomretro.com>
2021-08-13 16:05:10 +1000

    Implement MBF21's instant death sector special
    Support for MBF21 begins!

--- src/p_spec.h
+++ src/p_spec.h
@@ -635,6 +635,13 @@ dboolean EV_SilentLineTeleport(line_t *line, int side, mobj_t *thing, dboolean r
 #define FRICTION_MASK           0x0100
 #define PUSH_MASK               0x0200
 
+// reserved by BOOM spec - not implemented?
+// bit 10: suppress all sounds within the sector
+// bit 11: disable any sounds due to floor or ceiling motion by the sector
+
+// mbf21
+#define DEATH_MASK              0x1000  // bit 12
+
 // jff 02/04/98 Define masks, shifts, for fields in generalized linedef types
 #define GenEnd                  0x8000
 #define GenFloorBase            0x6000
