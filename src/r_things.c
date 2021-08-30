c5e74a61763d3ece5e9f51ef54f4d10297ec79f5
Brad Harding <brad@doomretro.com>
2021-08-30 16:14:17 +1000

    Minor optimization to drawing sprites/blood splats

--- src/r_things.c
+++ src/r_things.c
@@ -958,7 +958,7 @@ void R_AddSprites(sector_t *sec, int lightlevel)
             if (lightlevel != prevlightlevel)
             {
                 spritelights = scalelight[BETWEEN(0, (lightlevel >> LIGHTSEGSHIFT) + extralight, LIGHTLEVELS - 1)];
-                nextspritelights = (r_ditheredlighting ?
+                nextspritelights = (r_ditheredlighting && thing ?
                     scalelight[BETWEEN(0, ((lightlevel + 4) >> LIGHTSEGSHIFT) + extralight, LIGHTLEVELS - 1)] : spritelights);
                 prevlightlevel = lightlevel;
             }
