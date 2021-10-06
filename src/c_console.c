36d5419706da3b6c7a97c4841d6d078df00ef0a0
Brad Harding <brad@doomretro.com>
2021-10-06 15:41:19 +1100

    Fix positioning of multiple overlays in external automap

--- src/c_console.c
+++ src/c_console.c
@@ -1202,7 +1202,8 @@ void C_UpdatePathOverlay(void)
     {
         pathoverlay = true;
         C_DrawOverlayText(mapscreen, MAPWIDTH, MAPWIDTH - C_OverlayWidth(temp, false) - OVERLAYTEXTX + 1,
-            OVERLAYTEXTY + OVERLAYLINEHEIGHT * (vid_showfps + !!countdown), temp, consoleoverlaycolor, true);
+            OVERLAYTEXTY + OVERLAYLINEHEIGHT * ((vid_showfps && automapactive) + (!!countdown && automapactive)),
+            temp, consoleoverlaycolor, true);
         free(temp);
     }
 }
@@ -1210,7 +1211,7 @@ void C_UpdatePathOverlay(void)
 void C_UpdatePlayerStatsOverlay(void)
 {
     int     x = MAPWIDTH - OVERLAYTEXTX + 1;
-    int     y = OVERLAYTEXTY + OVERLAYLINEHEIGHT * (vid_showfps + !!countdown + pathoverlay);
+    int     y = OVERLAYTEXTY + OVERLAYLINEHEIGHT * ((vid_showfps && automapactive) + (!!countdown && automapactive) + pathoverlay);
     char    buffer[32];
     char    *temp1;
     char    *temp2;
