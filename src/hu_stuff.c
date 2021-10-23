883eacd348c778f89a13566e60a14688e277b2f7
Brad Harding <brad@doomretro.com>
2021-10-23 18:24:45 +1100

    Fix previous commit

--- src/hu_stuff.c
+++ src/hu_stuff.c
@@ -743,12 +743,12 @@ static int      yellow;
 
 static void HU_AltInit(void)
 {
-    char        buffer[8];
+    char        buffer[9];
     patch_t     *altkeypatch;
     patch_t     *altskullpatch;
     dboolean    weaponschanged = false;
 
-    for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 10; i++)
     {
         M_snprintf(buffer, sizeof(buffer), "DRHUD%iA", i);
         altnum[i] = W_CacheLumpName(buffer);
