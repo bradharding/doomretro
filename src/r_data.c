10a49bda7968537eabbc831ec59291d6047a87f5
Brad Harding <brad@doomretro.com>
2021-10-24 12:08:50 +1100

    Fix minor typo

--- src/r_data.c
+++ src/r_data.c
@@ -791,8 +791,8 @@ static void R_InitSpriteLumps(void)
         M_StringCopy(mobjinfo[MT_MISC38].plural1, "puppies", sizeof(mobjinfo[MT_MISC38].plural1));
         M_StringCopy(mobjinfo[MT_MISC65].name1, "eye spawner", sizeof(mobjinfo[MT_MISC65].name1));
         M_StringCopy(mobjinfo[MT_MISC65].plural1, "eye spawners", sizeof(mobjinfo[MT_MISC65].plural1));
-        M_StringCopy(mobjinfo[MT_MISC78].name1, "skelespider", sizeof(mobjinfo[MT_PLASMA].name1));
-        M_StringCopy(mobjinfo[MT_MISC78].plural1, "skelespiders", sizeof(mobjinfo[MT_PLASMA].plural1));
+        M_StringCopy(mobjinfo[MT_MISC78].name1, "skelespider", sizeof(mobjinfo[MT_MISC78].name1));
+        M_StringCopy(mobjinfo[MT_MISC78].plural1, "skelespiders", sizeof(mobjinfo[MT_MISC78].plural1));
     }
 }
 
