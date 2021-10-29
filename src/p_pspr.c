dbb98c9f598297a8f3ab175b5ac18079208f6f9f
Brad Harding <brad@doomretro.com>
2021-10-29 21:01:15 +1100

    Fix no red plasma balls when using A_FireOldBFG

--- src/p_pspr.c
+++ src/p_pspr.c
@@ -526,11 +526,13 @@ void A_FireBFG(mobj_t *actor, player_t *player, pspdef_t *psp)
 //
 void A_FireOldBFG(mobj_t *actor, player_t *player, pspdef_t *psp)
 {
+    mobjtype_t  type = MT_PLASMA1;
+
     P_SubtractAmmo(1);
 
     player->extralight = 2;
 
-    for (mobjtype_t type = MT_PLASMA1; type != MT_PLASMA2; type = MT_PLASMA2)
+    do
     {
         mobj_t  *th;
         angle_t an = actor->angle;
@@ -586,7 +588,7 @@ void A_FireOldBFG(mobj_t *actor, player_t *player, pspdef_t *psp)
         th->interpolate = -1;
 
         P_CheckMissileSpawn(th);
-    }
+    } while (type != MT_PLASMA2 && (type = MT_PLASMA2));  // killough: obfuscated!
 
     A_Recoil(wp_plasma);
 }
