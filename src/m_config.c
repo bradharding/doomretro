86194f4f6da31042618f8b7a713d93cf795dc5b7
Brad Harding <brad@doomretro.com>
2021-11-10 16:05:34 +1100

    Fix previous commit

--- src/m_config.c
+++ src/m_config.c
@@ -971,10 +971,11 @@ static void M_CheckCVARs(dboolean ispackageconfig)
     if (!M_StringCompare(vid_scaleapi, vid_scaleapi_software)
 #if defined(_WIN32)
         && !M_StringCompare(vid_scaleapi, vid_scaleapi_direct3d)
-#endif
-        && !M_StringCompare(vid_scaleapi, vid_scaleapi_opengl)
+#else
         && !M_StringCompare(vid_scaleapi, vid_scaleapi_opengles)
-        && !M_StringCompare(vid_scaleapi, vid_scaleapi_opengles2))
+        && !M_StringCompare(vid_scaleapi, vid_scaleapi_opengles2)
+#endif
+        && !M_StringCompare(vid_scaleapi, vid_scaleapi_opengl))
         vid_scaleapi = vid_scaleapi_default;
 
     if (!M_StringCompare(vid_scalefilter, vid_scalefilter_linear)
