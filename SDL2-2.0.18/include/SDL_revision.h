2c1ad856f023295142304dbe14ee2e653fc5a500
Brad Harding <brad@doomretro.com>
2021-11-28 09:21:47 +1100

    Update SDL libraries

--- SDL2-2.0.18/include/SDL_revision.h
+++ SDL2-2.0.18/include/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@cb2f3bedf5919af5b689214a79128cad2831cedb"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@c2904c8ce70cb1c295740ab5d65d36d226ead11f"
 #define SDL_REVISION_NUMBER 0
