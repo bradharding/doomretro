0e0ccb6c0158f04ed61623b89c33b5536233bee5
Brad Harding <brad@doomretro.com>
2021-12-19 14:57:37 +1100

    Output Clang version in console if used

--- src/c_console.c
+++ src/c_console.c
@@ -2351,7 +2351,9 @@ void C_PrintCompileDate(void)
             dayofweek(day, month + 1, year), months[month], day, year);
     }
 
-#if defined(_MSC_FULL_VER)
+#if defined(__clang__)
+    C_Output("It was compiled using " ITALICS("Clang v%i.%i.%i."), __clang_major__, __clang_minor__, __clang_patchlevel__);
+#elif defined(_MSC_FULL_VER)
     if (_MSC_BUILD)
         C_Output("It was compiled using v%i.%02i.%i.%i of the " ITALICS("Microsoft C/C++ Optimizing Compiler."),
             _MSC_FULL_VER / 10000000, (_MSC_FULL_VER % 10000000) / 100000, _MSC_FULL_VER % 100000, _MSC_BUILD);
