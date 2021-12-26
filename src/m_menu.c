dda320a1585b6aab9a0ebc95395d7e9b48dde39f
Brad Harding <brad@doomretro.com>
2021-12-26 17:30:37 +1100

    No fade effect unless toggling detail in options menu

--- src/m_menu.c
+++ src/m_menu.c
@@ -3460,7 +3460,8 @@ dboolean M_Responder(event_t *ev)
                         if (currentMenu != &NewDef || itemOn == 4)
                             S_StartSound(NULL, sfx_pistol);
 
-                        if (currentMenu != &NewDef && currentMenu != &SaveDef)
+                        if (currentMenu != &NewDef && currentMenu != &SaveDef
+                            && (currentMenu != &OptionsDef || (currentMenu == &OptionsDef && itemOn == 2)))
                             D_FadeScreen(false);
                     }
 
