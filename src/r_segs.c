503ab10378a1320159a24146f0a89c1ff055e6ba
Brad Harding <brad@doomretro.com>
2022-02-14 15:50:06 +1100

    Hopefully fix segfault in R_RenderSegLoop()
    See issue #695

--- src/r_segs.c
+++ src/r_segs.c
@@ -298,8 +298,12 @@ void R_RenderMaskedSegRange(drawseg_t *ds, const int x1, const int x2)
                 int index = MIN(spryscale >> LIGHTSCALESHIFT, MAXLIGHTSCALE - 1);
 
                 dc_colormap[0] = walllights[index];
-                dc_nextcolormap[0] = walllightsnext[index];
-                dc_z = spryscale;
+
+                if (r_ditheredlighting)
+                {
+                    dc_nextcolormap[0] = walllightsnext[index];
+                    dc_z = ((spryscale >> 5) & 255);
+                }
             }
 
             dc_iscale = UINT_MAX / (unsigned int)spryscale;
@@ -378,8 +382,12 @@ static void R_RenderSegLoop(void)
                 int index = MIN(rw_scale >> LIGHTSCALESHIFT, MAXLIGHTSCALE - 1);
 
                 dc_colormap[0] = walllights[index];
-                dc_nextcolormap[0] = walllightsnext[index];
-                dc_z = rw_scale;
+
+                if (r_ditheredlighting)
+                {
+                    dc_nextcolormap[0] = walllightsnext[index];
+                    dc_z = ((rw_scale >> 5) & 255);
+                }
             }
 
             dc_x = rw_x;
