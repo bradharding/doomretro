6b8fa7c5ad8ec5534ee38ecc405f754b965d4eb8
Brad Harding <brad@doomretro.com>
2022-07-13 17:12:58 +1000

    Update to SDL_mixer 2.6.1

--- SDL2_mixer-2.6.1/include/SDL_mixer.h
+++ SDL2_mixer-2.6.1/include/SDL_mixer.h
@@ -46,7 +46,7 @@ extern "C" {
  */
 #define SDL_MIXER_MAJOR_VERSION 2
 #define SDL_MIXER_MINOR_VERSION 6
-#define SDL_MIXER_PATCHLEVEL    0
+#define SDL_MIXER_PATCHLEVEL    1
 
 /**
  * This macro can be used to fill a version structure with the compile-time
