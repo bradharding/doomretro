3895034daa0bfccd3a6854a286eda1de5f28a246
Brad Harding <brad@doomretro.com>
2022-09-04 10:16:07 +1000

    Fix condump CCMD now that 1st line isn't always blank

--- src/c_cmds.c
+++ src/c_cmds.c
@@ -1917,7 +1917,7 @@ static void condump_cmd_func2(char *cmd, char *parms)
     {
         char    *temp = commify((int64_t)consolestrings - 2);
 
-        for (int i = 1; i < consolestrings - 1; i++)
+        for (int i = 0; i < consolestrings - 1; i++)
             if (console[i].stringtype == dividerstring)
                 fprintf(file, "%s\n", DIVIDERSTRING);
             else
