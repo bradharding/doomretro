782cc334b79435d29dd5b8d90ca55fa6e753dbdf
Brad Harding <brad@doomretro.com>
2022-10-21 19:23:20 +1100

    Fix spawning of unwanted blood splats in Harmony

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1265,7 +1265,7 @@ mobj_t *P_SpawnMapThing(mapthing_t *mthing, const bool spawnmonsters)
         && r_bloodsplats_max
         && !(flags & (MF_SHOOTABLE | MF_NOBLOOD | MF_SPECIAL))
         && mobj->bloodcolor > 0
-        && (!hacx || !(flags2 & MF2_DECORATION))
+        && ((!hacx && !harmony) || !(flags2 & MF2_DECORATION))
         && mobj->subsector->sector->terraintype == SOLID)
     {
         const short lump = sprites[mobj->sprite].spriteframes[mobj->frame & FF_FRAMEMASK].lump[0];
