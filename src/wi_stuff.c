aa546e4b8f6fff92a16c073c97b5edfa7ec9c23f
Brad Harding <brad@doomretro.com>
2022-12-02 14:55:53 +1100

    Check `sucktime` CVAR when outputting to console during intermission

--- src/wi_stuff.c
+++ src/wi_stuff.c
@@ -851,10 +851,10 @@ static void WI_InitStats(void)
         free(temp2);
     }
 
-    if (wbs->stime / TICRATE < 61 * 59)
-        C_TabbedOutput(tabs, "Time\t" BOLD("%02i:%02i"), wbs->stime / TICRATE / 60, wbs->stime / TICRATE % 60);
-    else
+    if (sucktime && wbs->stime / TICRATE > sucktime * 61 * 59)
         C_TabbedOutput(tabs, "Time\t" BOLD("%s"), s_STSTR_SUCKS);
+    else
+        C_TabbedOutput(tabs, "Time\t" BOLD("%02i:%02i"), wbs->stime / TICRATE / 60, wbs->stime / TICRATE % 60);
 
     if (wbs->partime)
         C_TabbedOutput(tabs, "Par time\t" BOLD("%02i:%02i"), wbs->partime / TICRATE / 60, wbs->partime / TICRATE % 60);
