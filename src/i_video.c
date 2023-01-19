34b85b3a374e120c0c03cb18668c845bda5c036b
Brad Harding <brad@doomretro.com>
2023-01-19 19:12:30 +1100

    Fix previous commit

--- src/i_video.c
+++ src/i_video.c
@@ -1932,12 +1932,13 @@ void I_InitGraphics(void)
     SDL_SetHintWithPriority(SDL_HINT_VIDEO_MINIMIZE_ON_FOCUS_LOSS, "1", SDL_HINT_OVERRIDE);
 #endif
 
-    if (vid_fullscreen)
-        SetShowCursor(false);
-
+    SDL_ShowCursor(false);
     SetVideoMode(true, true);
 
     I_CreateExternalAutomap();
+
+    if (vid_fullscreen)
+        SetShowCursor(false);
 
 #if defined(_WIN32)
     I_InitWindows32();
