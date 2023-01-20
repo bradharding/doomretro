1db6b7f7582cb672d4d7d9b27d1906df5f66700a
Brad Harding <brad@doomretro.com>
2023-01-20 16:07:44 +1100

    Fix if actual curly quotes used when stripping quotes

--- src/m_misc.c
+++ src/m_misc.c
@@ -940,7 +940,9 @@ void M_StripQuotes(char *str)
 {
     int len = (int)strlen(str);
 
-    if (len > 2 && ((str[0] == '"' && str[len - 1] == '"') || (str[0] == '\'' && str[len - 1] == '\'')))
+    if (len > 2
+        && (((str[0] == '"' || str[0] == '\x93') && (str[len - 1] == '"' || str[len - 1] == '\x94'))
+            || ((str[0] == '\'' || str[0] == '\x91') && (str[len - 1] == '\'' || str[len - 1] == '\x92'))))
     {
         len -= 2;
         memmove(str, str + 1, len);
