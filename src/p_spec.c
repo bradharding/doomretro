38012e1415724965df8be695d0866ed2fae3e1b0
Brad Harding <brad@doomretro.com>
2023-02-14 18:25:41 +1100

    Synchronize timer overlay with maptime overlay in automap

--- src/p_spec.c
+++ src/p_spec.c
@@ -2467,7 +2467,7 @@ bool EV_DoDonut(line_t *line)
 void P_SetTimer(int minutes)
 {
     timer = minutes;
-    timeremaining = timer * 60 * TICRATE;
+    timeremaining = timer * 60 * TICRATE + (maptime & TICRATE);
 }
 
 //
