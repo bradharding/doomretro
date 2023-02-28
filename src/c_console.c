570aa70aa330ea79ceeeada2b5e0faaace00ea5d
David CARLIER <devnexen@gmail.com>
2023-02-28 22:01:37 +0000

    C_Init memory leak fix

--- src/c_console.c
+++ src/c_console.c
@@ -636,11 +636,14 @@ void C_ClearConsole(void)
 
 void C_Init(void)
 {
-    const char  *appdatafolder = M_GetAppDataFolder();
+    char  *appdatafolder;
     char        consolefolder[MAX_PATH];
 
+    appdatafolder = M_GetAppDataFolder();
     M_snprintf(consolefolder, sizeof(consolefolder), "%s" DIR_SEPARATOR_S DOOMRETRO_CONSOLEFOLDER, appdatafolder);
     M_MakeDirectory(consolefolder);
+ 
+    free(appdatafolder);
 
     for (int i = 0, j = CONSOLEFONTSTART; i < CONSOLEFONTSIZE; i++)
     {
