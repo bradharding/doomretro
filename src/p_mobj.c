57972a1ebf169aea4f08e9305b678002c8736116
Brad Harding <brad@doomretro.com>
2023-02-28 17:34:16 +1100

    Improve random start frames when `r_randomstartframes` CVAR `on`

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1289,13 +1289,15 @@ mobj_t *P_SpawnMapThing(mapthing_t *mthing, const bool spawnmonsters)
     // so groups of same mobjs are deliberately out of sync with each other
     if (info->frames > 1 && r_randomstartframes)
     {
-        const int   numframes = M_BigRandomInt(0, info->frames);
+        int         prevnumframes = 0;
+        const int   numframes = M_BigRandomIntNoRepeat(0, info->frames, prevnumframes);
         state_t     *st = mobj->state;
 
         for (int j = 0; j < numframes && st->nextstate != S_NULL; j++)
             st = &states[st->nextstate];
 
         mobj->state = st;
+        prevnumframes = numframes;
     }
 
     // [BH] set random pitch for monster sounds when spawned
