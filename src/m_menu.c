96f4c1f8bbecbf53e53659379a96922c26f8646c
Brad Harding <brad@doomretro.com>
2023-04-23 07:49:09 +1000

    Further improvement to using sliders in options menu

--- src/m_menu.c
+++ src/m_menu.c
@@ -2756,7 +2756,7 @@ bool M_Responder(event_t *ev)
                                     key = 0;
                                 else if (itemon == sound_empty1 || itemon == sound_empty2)
                                     key = (ev->data2 < SoundMenu[itemon].sliderx ? KEY_LEFTARROW :
-                                        (ev->data2 > SoundMenu[itemon].sliderx + 8 ? KEY_RIGHTARROW : 0));
+                                        (ev->data2 > SoundMenu[itemon].sliderx + 6 ? KEY_RIGHTARROW : 0));
                                 else if (mousewait < I_GetTime())
                                 {
                                     key = KEY_ENTER;
