5af3229b3202c3c0777e4df194a48265c9e951e1
Brad Harding <brad@doomretro.com>
2023-05-14 10:14:09 +1000

    Fix spawning of blood if DeHackEd such as in SD21.WAD

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1460,11 +1460,10 @@ void P_SpawnBlood(const fixed_t x, const fixed_t y, const fixed_t z, angle_t ang
             th->angle = angle;
             angle += M_BigSubRandom() * ANGLEMULTIPLIER;
 
-            if (damage <= 12 && th->state->nextstate != S_NULL)
-                P_SetMobjState(th, th->state->nextstate);
-
-            if (damage < 9 && th->state->nextstate != S_NULL)
-                P_SetMobjState(th, th->state->nextstate);
+            if (damage <= 12 && damage >= 9)
+                P_SetMobjState(th, S_BLOOD2);
+            else if (damage < 9)
+                P_SetMobjState(th, S_BLOOD3);
         }
     }
 }
