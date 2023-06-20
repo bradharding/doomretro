bradharding/doomretro

commit c70b0d2b2bfa4da82a0e608a47cd62939e2f1e2d

Let menu handle events before console

src/m_menu.c
@@ -2941,6 +2941,8 @@ bool M_Responder(event_t *ev)
 
         if (consoleheight < CONSOLEHEIGHT && consoledirection == -1 && !dowipe)
             C_ShowConsole();
+        else
+            C_HideConsole();
 
         return true;
     }

src/c_console.c
@@ -1924,13 +1924,6 @@ bool C_Responder(event_t *ev)
         const int           key = ev->data1;
         const SDL_Keymod    modstate = SDL_GetModState();
 
-        if (key == keyboardconsole && !keydown)
-        {
-            keydown = key;
-            C_HideConsole();
-            return true;
-        }
-
         switch (key)
         {
             case KEY_PRINTSCREEN:

src/d_main.c
@@ -184,11 +184,11 @@ void D_PostEvent(event_t *ev)
 
     lasteventtype = ev->type;
 
-    if (C_Responder(ev))
-        return; // console ate the event
-
     if (M_Responder(ev))
         return; // menu ate the event
+
+    if (C_Responder(ev))
+        return; // console ate the event
 
     G_Responder(ev);
 }

commit d0690f0483fa15509dd59e61b350e7c16403c0e2

Fix entering +/- in console
There may be other instances, caused by c70b0d2b2bfa4da82a0e608a47cd62939e2f1e2d.

src/m_menu.c
@@ -3147,7 +3147,7 @@ bool M_Responder(event_t *ev)
     if (key == KEY_MINUS)
     {
         // Screen size down
-        if (paused || splashscreen || automapactive || helpscreen
+        if (consoleactive || paused || splashscreen || automapactive || helpscreen
             || gamestate == GS_INTERMISSION || gamestate == GS_FINALE)
             return false;
 
@@ -3174,7 +3174,7 @@ bool M_Responder(event_t *ev)
     else if (key == KEY_EQUALS)
     {
         // Screen size up
-        if (paused || splashscreen || automapactive || helpscreen
+        if (consoleactive || paused || splashscreen || automapactive || helpscreen
             || gamestate == GS_INTERMISSION || gamestate == GS_FINALE)
             return false;
 