88965bac22255aa4edcad41f30f74586fc2cddc3
Brad Harding <brad@doomretro.com>
2023-07-30 19:13:37 +1000

    Allow more keys, buttons to open menu from title screen

--- src/g_game.c
+++ src/g_game.c
@@ -773,19 +773,13 @@ bool G_Responder(const event_t *ev)
             && !fadecount
             && ((ev->type == ev_keydown
                 && !keydown
-                && ev->data1 != KEY_PAUSE
-                && ev->data1 != KEY_SHIFT
-                && ev->data1 != KEY_ALT
-                && ev->data1 != KEY_NUMLOCK
                 && (ev->data1 < KEY_F1 || ev->data1 > KEY_F12)
                 && !((ev->data1 == KEY_ENTER || ev->data1 == KEY_TAB) && altdown)
                 && ev->data1 != keyboardscreenshot)
-            || (ev->type == ev_mouse && mousewait < I_GetTime() && (ev->data1 & MOUSE_LEFTBUTTON))
+            || (ev->type == ev_mouse && mousewait < I_GetTime())
             || (ev->type == ev_controller
                 && gamecontrollerwait < I_GetTime()
-                && gamecontrollerbuttons
-                && !(gamecontrollerbuttons & (GAMECONTROLLER_B | GAMECONTROLLER_DPAD_UP | GAMECONTROLLER_DPAD_DOWN
-                    | GAMECONTROLLER_DPAD_LEFT | GAMECONTROLLER_DPAD_RIGHT)))))
+                && gamecontrollerbuttons)))
         {
             if (ev->type == ev_keydown && ev->data1 == keyboardalwaysrun)
             {
