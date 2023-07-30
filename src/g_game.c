cfb6ba2de9ab55825e942c870cd6313c1660d433
Brad Harding <brad@doomretro.com>
2023-07-30 19:17:48 +1000

    Fix previous commit

--- src/g_game.c
+++ src/g_game.c
@@ -776,7 +776,7 @@ bool G_Responder(const event_t *ev)
                 && (ev->data1 < KEY_F1 || ev->data1 > KEY_F12)
                 && !((ev->data1 == KEY_ENTER || ev->data1 == KEY_TAB) && altdown)
                 && ev->data1 != keyboardscreenshot)
-            || (ev->type == ev_mouse && mousewait < I_GetTime())
+            || (ev->type == ev_mouse && mousewait < I_GetTime() && ev->data1)
             || (ev->type == ev_controller
                 && gamecontrollerwait < I_GetTime()
                 && gamecontrollerbuttons)))
