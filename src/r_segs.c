98aece77ece643b2eb19762e96ea3ced729ef0c4
Brad Harding <brad@doomretro.com>
2023-08-20 10:41:30 +1000

    Revert attempted fix of wall textures next to liquid sectors
    Reverts 20409ce4fabd3204c69b46f770bda088cf0d2600. Will need to find another solution.

--- src/r_segs.c
+++ src/r_segs.c
@@ -855,7 +855,7 @@ void R_StoreWallRange(const int start, const int stop)
                 rw_bottomtexturemid = ((linedef->flags & ML_DONTPEGBOTTOM) ? worldtop : worldlow - liquidoffset)
                     + FixedMod(sidedef->rowoffset, height);
 
-                if (liquidoffset && !(linedef->flags & ML_TWOSIDED))
+                if (liquidoffset)
                     rw_bottomtexturemid += 4 * FRACUNIT;
             }
         }
