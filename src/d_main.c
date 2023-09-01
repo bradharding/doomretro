c96c477799851c9b1721cf534e98c4d861038945
Brad Harding <brad@doomretro.com>
2023-09-01 19:18:00 +1000

    Slight slowdown to splash screen

--- src/d_main.c
+++ src/d_main.c
@@ -537,13 +537,13 @@ void D_PageTicker(void)
 //
 void D_SplashDrawer(void)
 {
+    I_Sleep(20);
     gamestate = GS_TITLESCREEN;
     memset(screens[0], BLACK, SCREENAREA);
     V_DrawBigPatch(logox, logoy, logowidth, logoheight, logolump[BETWEEN(0, 94 - logotic, 17)]);
     V_DrawBigPatch(fineprintx, fineprinty, fineprintwidth, fineprintheight, fineprintlump);
     I_SetPalette(&splashpal[pagetic < 9 ? (9 - pagetic) * 768 : (pagetic <= 94 ? 0 : (pagetic - 94) * 768)]);
     blitfunc();
-    I_Sleep(1);
 }
 
 //
