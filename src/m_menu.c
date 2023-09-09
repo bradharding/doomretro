4349d4e46bd0bf1be9313b118f34726bb67a3c89
Brad Harding <brad@doomretro.com>
2023-09-09 16:31:58 +1000

    Minor tweaks to menu border

--- src/m_menu.c
+++ src/m_menu.c
@@ -497,8 +497,16 @@ static void M_DrawMenuBorder(void)
 
     for (int x = SCREENWIDTH * 3; x < SCREENWIDTH * 4; x++)
     {
-        screens[0][x] = tinttab75[screens[0][x]];
-        screens[0][SCREENAREA - SCREENWIDTH * 7 + x] = tinttab75[screens[0][SCREENAREA - SCREENWIDTH * 7 + x]];
+        byte *dot = *screens + x;
+
+        *dot = tinttab50[*dot];
+    }
+
+    for (int x = SCREENWIDTH * 3; x < SCREENWIDTH * 4; x++)
+    {
+        byte    *dot = *screens + SCREENAREA - SCREENWIDTH * 7 + x;
+
+        *dot = tinttab50[*dot];
     }
 
     if (vid_widescreen || nowidescreen)
