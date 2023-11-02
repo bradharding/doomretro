7f5d8d062a2dbe94c2975275f8ea181446876ab9
Brad Harding <brad@doomretro.com>
2023-11-02 19:41:06 +1100

    Improvements to help screen background

--- src/m_menu.c
+++ src/m_menu.c
@@ -601,22 +601,34 @@ static void M_DrawHelpBackground(void)
 
     M_BigSeed(411);
 
-    for (int y = 0; y < SCREENAREA; y += 2 * SCREENWIDTH)
-        for (int x = 0; x < SCREENWIDTH; x += 2)
+    if (r_detail == r_detail_high)
+    {
+        for (int i = 0; i < SCREENAREA; i++)
         {
-            byte        *dot1 = *screens + y + x;
-            byte        *dot2 = dot1 + 1;
-            byte        *dot3 = dot2 + SCREENWIDTH;
-            byte        *dot4 = dot3 - 1;
-            const byte  color = colormaps[0][M_BigRandomInt(0, 3) * 256
-                            + blues[tinttab50[(tinttab50[(*dot1 << 8) + *dot2] << 8)
-                            + tinttab50[(*dot3 << 8) + *dot4]]]];
+            byte    *dot = *screens + i;
 
-            *dot1 = color;
-            *dot2 = color;
-            *dot3 = color;
-            *dot4 = color;
+            *dot = colormaps[0][M_BigRandomInt(0, 3) * 256 + blues[*dot]];
         }
+    }
+    else
+        for (int y = 0; y < SCREENAREA; y += 2 * SCREENWIDTH)
+            for (int x = 0; x < SCREENWIDTH; x += 2)
+            {
+                byte        *dot1 = *screens + y + x;
+                byte        *dot2 = dot1 + 1;
+                byte        *dot3 = dot2 + SCREENWIDTH;
+                byte        *dot4 = dot3 - 1;
+                const byte  color = colormaps[0][M_BigRandomInt(0, 3) * 256
+                                + blues[tinttab50[(tinttab50[(*dot1 << 8) + *dot2] << 8)
+                                + tinttab50[(*dot3 << 8) + *dot4]]]];
+
+                *dot1 = color;
+                *dot2 = color;
+                *dot3 = color;
+                *dot4 = color;
+            }
+
+    M_DrawMenuBorder();
 
     if (mapwindow)
         memset(mapscreen, nearestblack, MAPAREA);
