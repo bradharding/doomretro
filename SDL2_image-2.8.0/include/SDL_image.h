57ec5203f1acae0bcc0b34d23cede86b4a60e7ae
Brad Harding <brad@doomretro.com>
2023-12-06 15:58:10 +1100

    Update to SDL_image v2.8.0

--- SDL2_image-2.8.0/include/SDL_image.h
+++ SDL2_image-2.8.0/include/SDL_image.h
@@ -42,8 +42,8 @@ extern "C" {
  * Printable format: "%d.%d.%d", MAJOR, MINOR, PATCHLEVEL
  */
 #define SDL_IMAGE_MAJOR_VERSION 2
-#define SDL_IMAGE_MINOR_VERSION 7
-#define SDL_IMAGE_PATCHLEVEL    1
+#define SDL_IMAGE_MINOR_VERSION 8
+#define SDL_IMAGE_PATCHLEVEL    0
 
 /**
  * This macro can be used to fill a version structure with the compile-time
