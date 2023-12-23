2e21cdbe12e7358991191707baa07c4def206c30
Brad Harding <brad@doomretro.com>
2023-12-23 15:34:24 +1100

    Make sure name doesn't override proper `Name` entry

--- src/d_deh.c
+++ src/d_deh.c
@@ -2627,7 +2627,7 @@ static void deh_procThing(DEHFILE *fpin, const char *line)
         }
         else if ((string = M_StringCompare(key, "Plural3")))
             M_StringCopy(mobjinfo[indexnum].plural3, lowercase(trimwhitespace(strval)), sizeof(mobjinfo[0].plural3));
-        else if (*name)
+        else if (*name && !namechange)
         {
             M_StringCopy(mobjinfo[indexnum].name1, lowercase(trimwhitespace(name)), sizeof(mobjinfo[0].name1));
             M_snprintf(mobjinfo[indexnum].plural1, sizeof(mobjinfo[0].plural1), "%ss", mobjinfo[indexnum].name1);
