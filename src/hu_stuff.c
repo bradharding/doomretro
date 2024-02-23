5b8284e617adfd6926ae24cc78825e2e3dcc8b68
Brad Harding <brad@doomretro.com>
2024-02-23 15:45:35 +1100

    Include empty `DRHUDWP0` lump

--- src/hu_stuff.c
+++ src/hu_stuff.c
@@ -876,21 +876,16 @@ static void HU_AltInit(void)
                     lumps++;
             }
 
-            if (lumps >= NUMWEAPONS - 1)
+            if (lumps == NUMWEAPONS)
                 weaponschanged = false;
         }
 
         if (!weaponschanged)
-        {
-            if (W_CheckNumForName("DRHUDWP0") >= 0)
-                altweapon[wp_fist].patch = W_CacheLumpName("DRHUDWP0");
-
-            for (int i = 1; i < NUMWEAPONS; i++)
+            for (int i = 0; i < NUMWEAPONS; i++)
             {
                 M_snprintf(buffer, sizeof(buffer), "DRHUDWP%i", i);
                 altweapon[i].patch = W_CacheLumpName(buffer);
             }
-        }
     }
 
     gray = nearestcolors[GRAY1];
