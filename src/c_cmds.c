0fc8d76592fe280fd38131aa9fa867f329190567
Brad Harding <brad@doomretro.com>
2024-03-28 16:48:40 +1100

    Improve `freelook` CVAR description

--- src/c_cmds.c
+++ src/c_cmds.c
@@ -677,7 +677,7 @@ consolecmd_t consolecmds[] =
     CVAR_BOOL(flashkeys, "", "", bool_cvars_func1, bool_cvars_func2, CF_NONE, BOOLVALUEALIAS,
         "Toggles flashing the required keycard or skull key when you try to open a locked door."),
     CVAR_BOOL(freelook, mouselook, "", bool_cvars_func1, freelook_func2, CF_NONE, BOOLVALUEALIAS,
-        "Toggles freelook."),
+        "Toggles freely looking up and down using the mouse or a controller."),
     CCMD(freeze, "", "", game_func1, freeze_func2, true, "[" BOLD("on") "|" BOLD("off") "]",
         "Toggles freeze mode."),
     CVAR_TIME(gametime, "", "", null_func1, time_cvars_func2,
