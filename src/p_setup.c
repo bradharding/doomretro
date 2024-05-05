f23168722d6771121475ec833fc311f125660362
Brad Harding <brad@doomretro.com>
2024-05-05 10:35:44 +1000

    Fix map names not parsing in some instances
    Thanks S3M_XM! https://www.doomworld.com/forum/post/2796323

--- src/p_setup.c
+++ src/p_setup.c
@@ -3504,16 +3504,26 @@ static bool P_ParseMapInfo(const char *scriptname)
 
             if (sscanf(sc_String, "%i", &map) != 1 || map < 0 || map > 99)
             {
+                char    *temp = uppercase(sc_String);
+
                 if (gamemode == commercial)
                 {
-                    if (sscanf(sc_String, "MAP0%1i", &map) != 1 && sscanf(sc_String, "MAP%2i", &map) != 1)
+                    if (sscanf(temp, "MAP0%1i", &map) != 1 && sscanf(temp, "MAP%2i", &map) != 1)
+                    {
+                        free(temp);
                         continue;
+                    }
                 }
                 else
                 {
-                    if (sscanf(sc_String, "E%1iM%i", &ep, &map) != 2)
+                    if (sscanf(temp, "E%1iM%i", &ep, &map) != 2)
+                    {
+                        free(temp);
                         continue;
+                    }
                 }
+
+                free(temp);
             }
 
             if (map < 0 || map > 99)
