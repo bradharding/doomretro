3d366070bd6a9210a8e888c45246b4e4fc05a554
Brad Harding <brad@doomretro.com>
2024-08-15 17:59:34 +1000

    Show custom episode name in window caption

--- src/m_menu.c
+++ src/m_menu.c
@@ -1787,7 +1787,10 @@ void M_SetWindowCaption(void)
     {
         if (gamemode == commercial)
         {
-            if ((gamemission == doom2 && !nerve) || gamemission == pack_plut
+            if (customepisode)
+                M_snprintf(caption, sizeof(caption), "%s \xC2\xB7 %s \xC2\xB7 %s \xC2\xB7 %s",
+                    mapnumandtitle, *episodes[gameepisode - 1], gamedescription, DOOMRETRO_NAME);
+            else if ((gamemission == doom2 && !nerve) || gamemission == pack_plut
                 || gamemission == pack_tnt || M_StringEndsWith(gamedescription, ".wad"))
                 M_snprintf(caption, sizeof(caption), "%s \xC2\xB7 %s \xC2\xB7 %s",
                     mapnumandtitle, gamedescription, DOOMRETRO_NAME);
