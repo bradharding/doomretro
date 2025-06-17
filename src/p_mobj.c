8a1a7f8ecb929ce026575a34f6b20dd631ed4e98
Brad Harding <brad@doomretro.com>
2025-06-17 18:02:30 +1000

    Don't apply randomness to angle of non-shootable things

--- src/p_mobj.c
+++ src/p_mobj.c
@@ -1352,9 +1352,6 @@ mobj_t *P_SpawnMapThing(mapthing_t *mthing, const bool spawnmonsters)
 
     mobj->angle = ((mthing->angle % 45) ? mthing->angle * (ANG45 / 45) : ANG45 * (mthing->angle / 45));
 
-    if ((!(flags & MF_SHOOTABLE) && type != TeleportDestination) || type == Barrel)
-        mobj->angle += (M_SubRandom() << 20);
-
     // [BH] randomly mirror corpses
     if (flags & MF_CORPSE)
     {
