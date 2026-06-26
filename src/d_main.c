50e78c0a2e3b7b5298e1fae4c1caa0dbc7fd6a13
Brad Harding <brad@doomretro.com>
2026-06-27 06:09:18 +1000

    Keep player view spinning in menu background when console open

--- src/d_main.c
+++ src/d_main.c
@@ -247,7 +247,7 @@ static void D_UpdateQuitMenuSpin(void)
 {
     if (gamestate == GS_LEVEL && menuspin && viewplayer && viewplayer->mo
         && !(helpscreen || palettescreen)
-        && !consoleheight
+        && (!consoleheight || consoleoverlaymenu)
         && (menuactive && (((messagetoprint && !consoleactive) || !messagetoprint))))
         viewplayer->mo->angle += ANG1 / (menuspinspeed = MIN(menuspinspeed + 1, 512)) * 8 * menuspindirection;
 }
