f1ee461d0982161293cc5b62c0926d897d046950
Brad Harding <brad@doomretro.com>
2026-07-14 16:12:32 +1000

    Check for BIGDOOR1 in Doom only, not Doom 2

--- src/r_patch.c
+++ src/r_patch.c
@@ -624,7 +624,7 @@ static void CreateTextureCompositePatch(const int id)
                 if ((id == BIGDOOR7 || id == FIREBLU1 || id == SKY1 || id == STEP2)
                     && W_GetNumTextures(texture->name) == 1)
                     oy = 0;
-                else if (id == BIGDOOR1 && W_GetNumTextures(texture->name) == 1)
+                else if (id == BIGDOOR1 && gamemission == doom && W_GetNumTextures(texture->name) == 1)
                     oy += 32;
                 else if (countsincolumn[tx].patches > 1)
                 {
